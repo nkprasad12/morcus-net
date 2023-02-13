0512cc08fa8f412439698195332e86f45d23ca34
Nitin Prasad <nkprasa@gmail.com>
2023-02-12 22:08:42 -0600

    Return a parsed text object from the XML parser.

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # morcus-net
+
+To run the current XML parser:
+npx tsc && node build/common/perseus_parser.js
--- src/common/perseus_parser.ts
+++ src/common/perseus_parser.ts
@@ -5,53 +5,131 @@ const REPO_ROOT = '/home/nkprasad/morcus/morcus-net'
 const LATIN_ROOT = 'texts/latin'
 const DBG_ROOT = 'data/phi0448/phi001/phi0448.phi001.perseus-lat2.xml'
 
-function printDiv(div: any): void {
-    console.log(div.head)
-    console.log(div.type)
-    let x = 0;
-    for (const part of div.div) {
-      console.log(part.div)
-      x += 1;
-      if (x > 1) {
-        break;
-      }
+class Section {
+  constructor(readonly passage: string, readonly num: number) { }
+
+  toString(): string {
+    return `${this.num}: ${this.passage}`;
+  }
+}
+
+class Chapter {
+  constructor(readonly sections: Section[], readonly num: number) { }
+
+  toString(): string {
+    const results = []
+    results.push(`Chapter ${this.num}`);
+    for (const section of this.sections) {
+      results.push(`  ${section}`);
     }
+    return results.join('\n');
+  }
+}
+
+class Book {
+  constructor(readonly chapters: Chapter[], readonly num: number) { }
+
+  toString(): string {
+    const results = []
+    results.push('======')
+    results.push(`Book ${this.num}`);
+    results.push('======')
+    for (const chapter of this.chapters) {
+      results.push(`${chapter}\n`);
+    }
+    return results.join('\n');
+  }
+}
+
+class FullText {
+  constructor(readonly books: Book[]) { }
+
+  toString(): string {
+    return this.books.map((book) => book.toString()).join('\n\n');
+  }
 }
 
 function getAttr(element: any, name: string): any {
-    return element[`@_${name}`]
-}
-
-function hasAttr(element: any, name: string): boolean {
-    return element[`@_${name}`]
+  return element[`@_${name}`]
 }
 
 function isTextPart(element: any): boolean {
-    if (!hasAttr(element, 'type')) {
-        return false;
+  const elementType = getAttr(element, 'type');
+  if (!elementType) {
+    return false;
+  }
+  return elementType === 'textpart';
+}
+
+function assertTextType(textPart: any, textType: string): void {
+  const subtype = getAttr(textPart, 'subtype');
+  if (subtype !== textType) {
+    throw new Error(`Expected ${textType}, but got ${textPart}`)
+  }
+}
+
+function assertHasDiv(textPart: any) {
+  if (!textPart.div) {
+    console.log(textPart);
+    throw new Error(`Expected to have div element: ${textPart}`)
+  }
+}
+
+function getChildren(textPart: any) {
+  assertHasDiv(textPart);
+  if (typeof textPart.div[Symbol.iterator] !== 'function') {
+    return [textPart.div];
+  }
+  return textPart.div;
+}
+
+function parseSection(textPart: any): Section {
+  assertTextType(textPart, 'section');
+  const n = getAttr(textPart, 'n');
+  const text = textPart.p['#text'] || textPart.p;
+  return new Section(text, n);
+}
+
+function parseChapter(textPart: any): Chapter {
+  assertTextType(textPart, 'chapter');
+  const n = getAttr(textPart, 'n');
+  const children = getChildren(textPart);
+  return new Chapter(children.map(parseSection), n);
+}
+
+function parseBook(textPart: any): Book {
+  assertTextType(textPart, 'book');
+  const n = getAttr(textPart, 'n');
+  const children = getChildren(textPart);
+  return new Book(children.map(parseChapter), n);
+}
+
+function parseFullText(root: any): FullText {
+  const children = getChildren(root);
+  const books = [];
+  for (const part of children) {
+    if (!isTextPart(part)) {
+      console.log(`Expected textpart, but got ${part}`);
+      continue;
     }
-    return getAttr(element,  'type') === 'textpart';
+    const subtype = getAttr(part, 'subtype');
+    if (subtype !== 'book') {
+      console.log(`Expected textpart book, but got ${part}`);
+      continue;
+    }
+    books.push(parseBook(part));
+  }
+  return new FullText(books);
 }
 
-function printTextPart(textPart: any): void {
-    console.log(`${getAttr(textPart, 'subtype')} ${getAttr(textPart, 'n')}`)
-}
-
-function readLatinFile(relativePath: string): void {
+function readLatinFile(relativePath: string): FullText {
   const xmlFile = readFileSync(`${REPO_ROOT}/${LATIN_ROOT}/${relativePath}`)
   const options = {
     ignoreAttributes: false
   }
   const contents = new XMLParser(options).parse(xmlFile);
-  const contentRoot = contents.TEI.text.body.div.div;
-  // printDiv(contentRoot[0]);
-
-  for (const part of contentRoot) {
-    if (isTextPart(part)) {
-        printTextPart(part);
-        // TODO: Pri nt the rest rescursively
-    }
-  }
+  const contentRoot = contents.TEI.text.body.div;
+  return parseFullText(contentRoot);
 }
 
-readLatinFile(DBG_ROOT);
+console.log(readLatinFile(DBG_ROOT).toString());
