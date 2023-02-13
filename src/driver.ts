3523c1cdd8c3f788fa37c2d3e259ef4ba32b834b
Nitin Prasad <nkprasa@gmail.com>
2023-02-12 23:29:29 -0600

    Create simple web server to return parsed content.

--- src/driver.ts
+++ src/driver.ts
@@ -1,5 +1,7 @@
 import { readLatinFile } from './common/perseus_parser'
+import { startServer } from './server/main'
 
 const DBG_ROOT = 'data/phi0448/phi001/phi0448.phi001.perseus-lat2.xml'
 
-console.log(readLatinFile(DBG_ROOT).toString());
+const content = readLatinFile(DBG_ROOT).toString();
+startServer(content);
