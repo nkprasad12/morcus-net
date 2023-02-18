d1b8fcd1e9240b48666ff97fa680b00adc341c95
Nitin Prasad <nkprasa@gmail.com>
2023-02-18 00:17:04 -0600

    Update Pytype ignore directive.

--- src/py/utils/pipeline.py
+++ src/py/utils/pipeline.py
@@ -1,13 +1,13 @@
 import time
 
-from src.libs.latin_macronizer.macronizer_modified import Macronizer
+from src.libs.latin_macronizer.macronizer_modified import Macronizer  # pytype: disable=attribute-error
 
 from src.py.utils import perseus_parser
 
 
 def run(file_path: str) -> None:
     """Runs the processing pipeline on the input XML file."""
-    text_data = perseus_parser.parse_perseus_xml(file_path)  # type: ignore
+    text_data = perseus_parser.parse_perseus_xml(file_path)
     macronizer = Macronizer()
     start_time = time.time()
     for i, part in enumerate(text_data):
