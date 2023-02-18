616b2d079b03585f7a867d97d6ef9218912694ed
Nitin Prasad <nkprasa@gmail.com>
2023-02-18 00:22:25 -0600

    Silence Pytype.

--- src/py/utils/pipeline.py
+++ src/py/utils/pipeline.py
@@ -1,8 +1,9 @@
 import time
 
-from src.libs.latin_macronizer.macronizer_modified import (
-    Macronizer,
-)  # pytype: disable=attribute-error
+# pytype: disable=import-error
+from src.libs.latin_macronizer.macronizer_modified import Macronizer
+
+# pytype: enable=import-error
 
 from src.py.utils import perseus_parser
 
