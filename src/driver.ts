0b53b2607596a24e0a4d584ae2a3dea0a8fdc1ad
Nitin Prasad <nkprasa@gmail.com>
2023-03-01 18:05:24 -0600

    Serve HTML from node web server

--- src/driver.ts
+++ src/driver.ts
@@ -1,8 +1,3 @@
-import { readFile } from "@/common/perseus_parser";
-import { startServer } from "@/server/main";
+import { startServer } from "@/web/main";
 
-const DBG_ROOT =
-  "texts/latin/perseus/data/phi0448/phi001/phi0448.phi001.perseus-lat2.xml";
-
-const content = readFile(DBG_ROOT).toString();
-startServer(content);
+startServer();
