cf3d99e3f7ca0b2f50cda03b8f4637421423b288
Nitin Prasad <nkprasa@gmail.com>
2023-03-03 15:53:31 -0600

    Refactor to inject dependencies.

--- src/driver.ts
+++ src/driver.ts
@@ -1,8 +1,23 @@
 /* istanbul ignore file */
 
+import express from "express";
+import http from "http";
+
 import * as dotenv from "dotenv";
 
-import { startServer } from "@/web/web_server";
+import { createProcessorConnection } from "@/web/processor_connection";
+import { startServer, WebServerParams } from "@/web/web_server";
 
 dotenv.config();
-startServer();
+
+const app = express();
+const server = http.createServer(app);
+const processorConnection = createProcessorConnection(server);
+
+const params: WebServerParams = {
+  app: app,
+  server: server,
+  macronizer: processorConnection.process,
+};
+
+startServer(params);
--- src/web/web_server.ts
+++ src/web/web_server.ts
@@ -1,13 +1,5 @@
 import express, { Request } from "express";
-import expressStaticGzip from "express-static-gzip";
 import http from "http";
-import { Server, Socket } from "socket.io";
-
-import {
-  PROCESSED_OUTPUT_CHANNEL,
-  ProcessingMessage,
-  RAW_INPUT_CHANNEL,
-} from "@/web/nlp/types";
 
 const host = "localhost";
 const port = 8000;
@@ -16,75 +8,22 @@ function log(message: string) {
   console.log(`[Web Server] ${message}`);
 }
 
-class ProcessingConnection {
-  private readonly pendingRequests: Map<string, (output: string) => any> =
-    new Map();
-
-  private availableId: number = 0;
-  private socket: Socket | undefined = undefined;
-
-  constructor(server: Server) {
-    server.on("connection", (socket: Socket) => {
-      log("Connected to processing server.");
-      this.socket = socket;
-      socket.on("disconnect", () => {
-        log("Disconnected from processing server.");
-        this.socket = undefined;
-      });
-      socket.on(PROCESSED_OUTPUT_CHANNEL, (message: ProcessingMessage) => {
-        log(`Got results for request: ${message.id}`);
-        const resolver = this.pendingRequests.get(message.id);
-        if (resolver === undefined) {
-          log("No resolver for result.");
-          return;
-        }
-        resolver(message.content);
-      });
-    });
-  }
-
-  async process(input: string): Promise<string> {
-    return new Promise((resolve) => {
-      if (this.socket === undefined) {
-        resolve("Processing backend is not available. Please try again later.");
-        return;
-      }
-      this.pendingRequests.set(`${this.availableId}`, resolve);
-      const message: ProcessingMessage = {
-        id: `${this.availableId}`,
-        content: input,
-      };
-      this.availableId += 1;
-      log(`Sending request ${message.id}`);
-      this.socket.emit(RAW_INPUT_CHANNEL, message);
-    });
-  }
+export interface WebServerParams {
+  server: http.Server;
+  app: express.Express;
+  macronizer: (input: string) => Promise<string>;
 }
 
-export function startServer(): void {
-  const app = express();
-  const server = http.createServer(app);
-  const contentRouter = express.Router();
-  contentRouter.use(expressStaticGzip("genfiles_static", { index: false }));
+export function startServer(params: WebServerParams): void {
+  const app = params.app;
+  const server = params.server;
+
   app.use(express.static("genfiles_static"));
-
-  const io = new Server(server);
-  io.use((socket, next) => {
-    const token = socket.handshake.auth.token;
-    if (token !== process.env.PROCESSING_SERVER_TOKEN) {
-      next(new Error("Unrecognized processing backend."));
-    } else {
-      log("Authenticated processing backend.");
-      next();
-    }
-  });
-  const processingConnection = new ProcessingConnection(io);
-
+  // TODO: Make the route a constant so that we can access it from the client.
   app.get(
     "/api/macronize/:input",
     async (req: Request<{ input: string }>, res) => {
-      const output = await processingConnection.process(req.params.input);
-      res.send(output);
+      res.send(await params.macronizer(req.params.input));
     }
   );
 
