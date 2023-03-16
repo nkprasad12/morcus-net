1eaf0c3d3a0f14467852fa9f2f37c9abcd412baa
Nitin Prasad <nkprasa@gmail.com>
2023-03-15 23:23:42 -0500

    Create new socket processing library

---
+++ src/web/workers/worker_types.test.ts
@@ -0,0 +1,11 @@
+import { Workers } from "./worker_types";
+
+describe("Workers.isValid", () => {
+  it("rejects invalid inputs", () => {
+    expect(Workers.isValid("Caesar")).toBe(false);
+  });
+
+  it("allows valid inputs", () => {
+    expect(Workers.isValid(Workers.MACRONIZER)).toBe(true);
+  });
+});
