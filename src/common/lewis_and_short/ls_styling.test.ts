0b4510750b43791769ce97d0156542145c399fb0
Nitin Prasad <nkprasa@gmail.com>
2023-03-25 15:25:55 -0500

    Add algorithm to substitute abbreviations

--- src/common/lewis_and_short/ls_styling.test.ts
+++ src/common/lewis_and_short/ls_styling.test.ts
@@ -2,6 +2,7 @@ import {
   substituteAbbreviation,
   attachHoverText,
   TrieNode,
+  attachAbbreviations,
 } from "./ls_styling";
 
 describe("TrieNode", () => {
@@ -15,8 +16,7 @@ describe("TrieNode", () => {
     const values = root.next("a")!.next("b")!.nodeValues();
     expect(values).toHaveLength(1);
     expect(root.next("a")!.next("b")!.isFullWord()).toBe(true);
-    expect(values[0].value).toBe("Caesar");
-    expect(values[0].tags).toStrictEqual(["Imperator"]);
+    expect(values[0]).toBe("Caesar");
   });
 
   it("handles disjoint adds", () => {
@@ -26,10 +26,10 @@ describe("TrieNode", () => {
 
     const aVals = root.next("a")!.nodeValues();
     expect(aVals).toHaveLength(1);
-    expect(aVals[0].value).toBe("Augustus");
+    expect(aVals[0]).toBe("Augustus");
     const jcVals = root.next("j")!.next("c")!.nodeValues();
     expect(jcVals).toHaveLength(1);
-    expect(jcVals[0].value).toBe("Caesar");
+    expect(jcVals[0]).toBe("Caesar");
   });
 
   it("handles duplicate adds", () => {
@@ -39,8 +39,8 @@ describe("TrieNode", () => {
 
     const jcVals = root.next("j")!.next("c")!.nodeValues();
     expect(jcVals).toHaveLength(2);
-    expect(jcVals[0].value).toBe("Caesar");
-    expect(jcVals[1].value).toBe("Augustus");
+    expect(jcVals[0]).toBe("Caesar");
+    expect(jcVals[1]).toBe("Augustus");
   });
 
   it("handles overlapping adds", () => {
@@ -50,10 +50,10 @@ describe("TrieNode", () => {
 
     const jcVals = root.next("j")!.next("c")!.nodeValues();
     expect(jcVals).toHaveLength(1);
-    expect(jcVals[0].value).toBe("Caesar");
+    expect(jcVals[0]).toBe("Caesar");
     const jcaVals = root.next("j")!.next("c")!.next("a")!.nodeValues();
     expect(jcaVals).toHaveLength(1);
-    expect(jcaVals[0].value).toBe("Augustus");
+    expect(jcaVals[0]).toBe("Augustus");
   });
 
   it("returns values with filters as subset", () => {
@@ -103,5 +103,77 @@ describe("substituteAbbreviation", () => {
 
     expect(result).toContain("Augustus");
     expect(result).toContain(`Expanded from: Caesar`);
+  });
+});
+
+describe("attachAbbreviations", () => {
+  const trieRoot = new TrieNode();
+
+  beforeAll(() => {
+    trieRoot.add("de Or.", "de Oratione");
+    trieRoot.add("v.", "verb");
+    trieRoot.add("v. h. v.", "vide hanc vocem");
+    trieRoot.add("t.", "testPost");
+    trieRoot.add("t. t.", "technical term");
+    trieRoot.add("q.", "qui");
+    trieRoot.add("q.", "quam");
+  });
+
+  it("is no-op on text with no substitutions", () => {
+    const input = "I have no substitutions.";
+    const output = attachAbbreviations(input, trieRoot);
+    expect(input).toStrictEqual(output);
+  });
+
+  it("handles multi-word keys", () => {
+    const input = "I have no de Or. substitutions.";
+    const output = attachAbbreviations(input, trieRoot);
+    const deOr = attachHoverText("de Oratione", "Expanded from: de Or.");
+    expect(output).toStrictEqual(`I have no ${deOr} substitutions.`);
+  });
+
+  it("handles multi-word keys", () => {
+    const input = "I q. hi";
+    const output = attachAbbreviations(input, trieRoot);
+    const ambig = attachHoverText("q.", "Ambiguous: qui OR quam");
+    expect(output).toStrictEqual(`I ${ambig} hi`);
+  });
+
+  it("handles ambiguous keys", () => {
+    const input = "I v. h. v. have no t. t. substitutions.";
+    const output = attachAbbreviations(input, trieRoot);
+    const vhv = attachHoverText("vide hanc vocem", "Expanded from: v. h. v.");
+    const tt = attachHoverText("technical term", "Expanded from: t. t.");
+    expect(output).toStrictEqual(`I ${vhv} have no ${tt} substitutions.`);
+  });
+
+  it("uses shortest keys if longer are not present", () => {
+    const input = "I v. have no t. substitutions.";
+    const output = attachAbbreviations(input, trieRoot);
+    const v = attachHoverText("verb", "Expanded from: v.");
+    const t = attachHoverText("testPost", "Expanded from: t.");
+    expect(output).toStrictEqual(`I ${v} have no ${t} substitutions.`);
+  });
+
+  it("handles abbreviations at start", () => {
+    const input = "v. h. v. have no substitutions.";
+    const output = attachAbbreviations(input, trieRoot);
+    const vhv = attachHoverText("vide hanc vocem", "Expanded from: v. h. v.");
+    expect(output).toStrictEqual(`${vhv} have no substitutions.`);
+  });
+
+  it("handles abbreviations at end", () => {
+    const input = "I have no substitutions t. t.";
+    const output = attachAbbreviations(input, trieRoot);
+    const tt = attachHoverText("technical term", "Expanded from: t. t.");
+    expect(output).toStrictEqual(`I have no substitutions ${tt}`);
+  });
+
+  it("handles substring edge case", () => {
+    const input = "I de v. hi.";
+    const output = attachAbbreviations(input, trieRoot);
+    expect(output).toStrictEqual(
+      `I de ${attachHoverText("verb", "Expanded from: v.")} hi.`
+    );
   });
 });
--- src/common/lewis_and_short/ls_styling.ts
+++ src/common/lewis_and_short/ls_styling.ts
@@ -26,11 +26,11 @@ export class TrieNode {
     return this.children.get(character);
   }
 
-  nodeValues(filters?: string[]): TrieValue[] {
-    const goodValues: TrieValue[] = [];
+  nodeValues(filters?: string[]): string[] {
+    const goodValues: string[] = [];
     for (const trieValue of this.values) {
       if (filters === undefined || filters.length === 0) {
-        goodValues.push(trieValue);
+        goodValues.push(trieValue.value);
         continue;
       }
       if (trieValue.tags === undefined || trieValue.tags.length === 0) {
@@ -42,7 +42,7 @@ export class TrieNode {
       if (!hasAllFilters) {
         continue;
       }
-      goodValues.push(trieValue);
+      goodValues.push(trieValue.value);
     }
     return goodValues;
   }
@@ -56,10 +56,62 @@ export class TrieNode {
 //
 // And also we can have multi-word keys like `de Or.` where we need to
 // make sure we are handling `de` as connected to `Or.`.
-// export function attachAbbreviations(
-//   message: string,
-//   lookup: Map<string, string>
-// ): string {}
+export function attachAbbreviations(
+  message: string,
+  trieRoot: TrieNode
+): string {
+  // [startIndex, length, expandedString]
+  const expansions: [number, number, string[]][] = [];
+  let triePosition: TrieNode = trieRoot;
+  let currentStart: number | undefined = undefined;
+  let bestExpansion: [number, number, string[]] | undefined = undefined;
+  for (let i = 0; i <= message.length; i++) {
+    const c = i === message.length ? "@@" : message[i];
+    const nextNode = triePosition.next(c);
+    if (nextNode === undefined) {
+      triePosition = trieRoot;
+      if (bestExpansion !== undefined) {
+        expansions.push(bestExpansion);
+        bestExpansion = undefined;
+      } else if (currentStart !== undefined) {
+        i = currentStart + 1;
+      }
+      currentStart = undefined;
+      continue;
+    }
+
+    if (currentStart === undefined) {
+      currentStart = i;
+    }
+    triePosition = nextNode;
+    if (triePosition.isFullWord()) {
+      bestExpansion = [
+        currentStart,
+        i - currentStart + 1,
+        triePosition.nodeValues(),
+      ];
+    }
+  }
+
+  const chunks: string[] = [];
+  let lastChunkEnd = 0;
+  for (const [startIndex, length, expandedString] of expansions) {
+    chunks.push(message.slice(lastChunkEnd, startIndex));
+    const original = message.slice(startIndex, startIndex + length);
+    lastChunkEnd = startIndex + length;
+    if (expandedString.length === 1) {
+      chunks.push(
+        attachHoverText(expandedString[0], `Expanded from: ${original}`)
+      );
+    } else {
+      chunks.push(
+        attachHoverText(original, `Ambiguous: ${expandedString.join(" OR ")}`)
+      );
+    }
+  }
+  chunks.push(message.slice(lastChunkEnd));
+  return chunks.join("");
+}
 
 export function attachHoverText(
   displayText: string,
