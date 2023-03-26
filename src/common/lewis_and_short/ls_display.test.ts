047aa4ac9ce70a5bef26fdc6a94044c00a01f367
Nitin Prasad <nkprasa@gmail.com>
2023-03-26 17:03:58 -0500

    Add simple test case for end-to-end entry

--- src/common/lewis_and_short/ls_display.test.ts
+++ src/common/lewis_and_short/ls_display.test.ts
@@ -5,9 +5,10 @@ import {
   displayUsg,
   formatSenseList,
 } from "./ls_display";
-import { XmlNode } from "./ls_parser";
+import { parseEntries, XmlNode } from "./ls_parser";
 
 const OL_OPEN = '<ol class="lsSenseList">';
+const CANABA = `<entryFree key="canaba" type="main" id="n6427"><orth lang="la" extent="full">cānăba</orth> (or <orth lang="la" extent="full">cannăba</orth>), <itype>ae</itype>, <gen>f.</gen> <etym>kindr. with <foreign lang="greek">κάναβος</foreign> and <foreign lang="greek">κάννα</foreign>; acc. to others, with <foreign lang="greek">καλύβη</foreign></etym>, <sense level="1" n="I" id="n6427.0"><hi rend="ital">a hovel</hi>, <hi rend="ital">hut</hi>, <bibl n="August. Serm. 61"><author>Aug.</author> Serm. 61</bibl>, de Temp.; <bibl><author>Inscr. Orell.</author> 39</bibl>; <bibl>4077</bibl>.</sense></entryFree>`;
 
 describe("displayNote", () => {
   it("is collapsed entirely", () => {
@@ -163,5 +164,24 @@ describe("defaultDisplay", () => {
     expect(child.name).toBe("span");
     expect(child.children).toHaveLength(1);
     expect(child.children[0]).toBe("bar");
+  });
+});
+
+describe("displayEntryFree", () => {
+  it("shows expected entry", () => {
+    const input = parseEntries([CANABA])[0];
+    const expected = [
+      '<span><span class="lsOrth">cānăba</span> (or <span class="lsOrth">cannăba</span>), ',
+      "<span>ae</span>, ",
+      '<span title="Expanded from: f." class="lsAbbrReplaced">feminine</span> ',
+      "<span>kindr. with <span>κάναβος</span> and <span>κάννα</span>; acc. to others, with <span>καλύβη</span></span>, ",
+      "<span><i><span>a hovel</span></i>, <i><span>hut</span></i>, ",
+      '<span><span title="Aurelius Augustinus, Christian writer, obiit, A.D. 430" class="lsAuthor">Aug.</span> ',
+      '<span title="Expanded from: Serm." class="lsWork">Sermones.</span> 61</span>, de Temp.; ',
+      '<span><span title="undefined" class="lsAuthor">Inscr. Orell.</span> 39</span>; <span>4077</span>.</span></span>',
+    ];
+
+    const output = defaultDisplay(input);
+    expect(output.toString()).toBe(expected.join(""));
   });
 });
