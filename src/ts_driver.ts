04c29e9c9a50a1f3b9b6b964c52b94ae66f14f07
Nitin Prasad <nkprasa@gmail.com>
2023-03-28 11:20:35 -0500

    Add parser for lamonpy model

--- src/ts_driver.ts
+++ src/ts_driver.ts
@@ -1,48 +1,29 @@
 /* istanbul ignore file */
 
 import * as dotenv from "dotenv";
-import { printElementsMatching } from "./common/lewis_and_short/ls_scripts";
+import { parse, XmlNode } from "./common/lewis_and_short/ls_parser";
 dotenv.config();
 
-// import { displayEntryFree } from "@/common/lewis_and_short/ls_display";
-// import { parseEntries } from "@/common/lewis_and_short/ls_parser";
-
-// import { printUniqueElementsMatching } from "@/common/lewis_and_short/ls_scripts";
-// printUniqueElementsMatching((node) => node.name === "usg");
-
-// const SAMPLE_ENTRY = `<entryFree key="canaba" type="main" id="n6427"><orth lang="la" extent="full">cānăba</orth> (or <orth lang="la" extent="full">cannăba</orth>), <itype>ae</itype>, <gen>f.</gen> <etym>kindr. with <foreign lang="greek">κάναβος</foreign> and <foreign lang="greek">κάννα</foreign>; acc. to others, with <foreign lang="greek">καλύβη</foreign></etym>, <sense level="1" n="I" id="n6427.0"><hi rend="ital">a hovel</hi>, <hi rend="ital">hut</hi>, <bibl n="August. Serm. 61"><author>Aug.</author> Serm. 61</bibl>, de Temp.; <bibl><author>Inscr. Orell.</author> 39</bibl>; <bibl>4077</bibl>.</sense></entryFree>`;
-// console.log(displayEntryFree(parseEntries([SAMPLE_ENTRY])[0]));
-
-printElementsMatching((node) => {
-  // if (node.name !== "entryFree") {
-  //   return false;
-  // }
-  // let lastSense = false;
-  // for (const child of node.children) {
-  //   if (typeof child === "string") {
-  //     lastSense = false;
-  //     continue;
-  //   }
-  //   if (child.name === "sense") {
-  //     lastSense = true;
-  //     // continue;
-  //     return true;
-  //   }
-  //   if (lastSense) {
-  //     return true;
-  //   }
-  //   lastSense = false;
-  // }
-  // return false;
-  if (node.name !== "sense") {
-    return false;
+function childrenMatching(node: XmlNode, name: string): XmlNode[] {
+  const results = [];
+  for (const child of node.children) {
+    if (typeof child !== "string" && child.name === name) {
+      results.push(child);
+    }
   }
-  const attrs = new Map(node.attrs);
-  if (!attrs.has("level")) {
-    return true;
+  return results;
+}
+
+let multiplePos = 0;
+let multipleEtym = 0;
+for (const entry of parse(process.env.LS_PATH!)) {
+  if (childrenMatching(entry, "pos").length > 1) {
+    multiplePos += 1;
   }
-  if (!attrs.has("n")) {
-    return true;
+  if (childrenMatching(entry, "etym").length > 1) {
+    multipleEtym += 1;
+    console.log(entry.attrs);
   }
-  return false;
-}, 10);
+}
+console.log(`multiplePos: ${multiplePos}`);
+console.log(`multipleEtym: ${multipleEtym}`);
