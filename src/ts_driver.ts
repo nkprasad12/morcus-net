cce73fedcf93462992d097fb9722c0dea6219674
Nitin Prasad <nkprasa@gmail.com>
2023-04-02 01:55:49 -0500

    add driver

--- src/ts_driver.ts
+++ src/ts_driver.ts
@@ -2,48 +2,46 @@
 
 import * as dotenv from "dotenv";
 import { checkPresent } from "./common/assert";
-import { displayEntryFree } from "./common/lewis_and_short/ls_display";
-import { parse, XmlNode } from "./common/lewis_and_short/ls_parser";
+import { parse } from "./common/lewis_and_short/ls_parser";
+import { XmlNode } from "./common/lewis_and_short/xml_node";
 dotenv.config();
 
-// @ts-ignore
-function childrenMatching(node: XmlNode, name: string): XmlNode[] {
-  const results = [];
-  for (const child of node.children) {
+function childrenMatching(node: XmlNode, name: string): [XmlNode, number][] {
+  const results: [XmlNode, number][] = [];
+  node.children.forEach((child, i) => {
     if (typeof child !== "string" && child.name === name) {
-      results.push(child);
+      results.push([child, i]);
     }
-  }
+  });
   return results;
 }
 
 // let multiplePos = 0;
 // let multipleEtym = 0;
-// for (const entry of parse(checkPresent(process.env.LS_PATH))) {
-//   if (childrenMatching(entry, "pos").length > 1) {
-//     multiplePos += 1;
-//   }
-//   if (childrenMatching(entry, "etym").length > 1) {
-//     multipleEtym += 1;
-//     console.log(entry.attrs);
-//   }
-// }
+// const counts = new Map<number, number>();
+for (const entry of parse(checkPresent(process.env.LS_PATH))) {
+  if (childrenMatching(entry, "pos").length > 1) {
+    console.log(
+      childrenMatching(entry, "pos").forEach((x) => console.log(x.toString()))
+    );
+    console.log("\n");
+    // multiplePos += 1;
+  }
+  // const etyms = childrenMatching(entry, "etym");
+  // const keyToIncr = etyms.length === 0 ? -1 : etyms[0][1];
+  // if (keyToIncr > 17) {
+  //   console.log(entry.formatAsString(true));
+  //   console.log("\n");
+  // }
+  // if (!counts.has(keyToIncr)) {
+  //   counts.set(keyToIncr, 0);
+  // }
+  // counts.set(keyToIncr, counts.get(keyToIncr)! + 1);
+  // if (etyms.length > 1) {
+  //   multipleEtym += 1;
+  //   console.log(entry.attrs);
+  // }
+}
 // console.log(`multiplePos: ${multiplePos}`);
 // console.log(`multipleEtym: ${multipleEtym}`);
-
-let errors = 0;
-let successes = 0;
-for (const entry of parse(checkPresent(process.env.LS_PATH))) {
-  try {
-    displayEntryFree(entry);
-    successes += 1;
-  } catch (e) {
-    errors += 1;
-    console.log("================");
-    console.log(entry.attrs);
-    console.log(e);
-    console.log("================");
-  }
-}
-console.log(`successes: ${successes}`);
-console.log(`errors: ${errors}`);
+// console.log(counts);
