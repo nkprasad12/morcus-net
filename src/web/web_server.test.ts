09a0bb29a8a0c0d02687b3f4817dd9ee3b3ddd26
Nitin Prasad <nkprasa@gmail.com>
2023-04-28 02:27:22 -0500

    Update test not to rely on genfiles

--- src/web/web_server.test.ts
+++ src/web/web_server.test.ts
@@ -1,11 +1,29 @@
 import { describe, expect, test } from "@jest/globals";
 import express from "express";
+import fs from "fs";
 import request from "supertest";
 
 import { entriesByPrefix, lsCall, macronizeCall } from "@/web/api_routes";
 import { setupServer, WebServerParams } from "./web_server";
+import path from "path";
 
 console.debug = jest.fn();
+
+const TEMP_FILE = "web_server.test.ts.html";
+
+function writeFile(contents: string) {
+  fs.writeFileSync(TEMP_FILE, contents);
+}
+
+beforeAll(() => {
+  writeFile("<!DOCTYPE html>\n<html></html>");
+});
+
+afterAll(() => {
+  try {
+    fs.unlinkSync(TEMP_FILE);
+  } catch (e) {}
+});
 
 function getServer(): express.Express {
   const app = express();
@@ -14,6 +32,7 @@ function getServer(): express.Express {
     macronizer: (a) => Promise.resolve(a + "2"),
     lsDict: (a) => Promise.resolve(`${a} def`),
     entriesByPrefix: (a) => Promise.resolve([a]),
+    indexFilePath: path.resolve(TEMP_FILE),
   };
   setupServer(params);
   return app;
