79649a3271da191bbb6c53d88858417f8feee70a
Nitin Prasad <nkprasa@gmail.com>
2023-04-28 10:53:37 -0500

    Add E2E test workflow

---
+++ src/integration/e2e_test.py
@@ -0,0 +1,27 @@
+import os
+import subprocess
+
+
+BUILD_CLIENT = "npm run build-client -- --env production"
+DOWNLOAD_LS = "npm run download-ls"
+PROCESS_LS = "npm run ts-node src/scripts/process_ls.ts -- --verify"
+
+
+def wait_and_check(proc: subprocess.Popen, message: str) -> None:
+    proc.wait()
+    if proc.returncode != 0:
+        raise RuntimeError(message)
+
+
+build_client = subprocess.Popen(BUILD_CLIENT, shell=True)
+download_ls = subprocess.Popen(DOWNLOAD_LS, shell=True)
+wait_and_check(download_ls, "Failed to download raw LS file")
+
+subproc_env = os.environ.copy()
+subproc_env["LS_PATH"] = "ls.xml"
+subproc_env["LS_PROCESSED_PATH"] = "lsp.txt"
+subproc_env["PORT"] = 5757
+
+process_ls = subprocess.Popen(PROCESS_LS, shell=True, env=subproc_env)
+wait_and_check(build_client, "Failed to build the client")
+wait_and_check(process_ls, "Failed to process or verify LS")
--- src/scripts/process_ls.ts
+++ src/scripts/process_ls.ts
@@ -1,10 +1,24 @@
 /* istanbul ignore file */
 
 import { LewisAndShort } from "@/common/lewis_and_short/ls";
+import { displayEntryFree } from "@/common/lewis_and_short/ls_display";
 import * as dotenv from "dotenv";
 dotenv.config();
 
 const startTime = performance.now();
-LewisAndShort.save(LewisAndShort.createProcessed());
+const verify = process.argv[2] && process.argv[2] === "--verify";
+if (verify) {
+  console.debug("Verifying displayEntryFree for all entries.");
+  const allProcessed = LewisAndShort.createProcessedRaw();
+  const result = [];
+  for (const item of allProcessed) {
+    result.push(LewisAndShort.processedToRaw(item));
+    displayEntryFree(item.entry);
+  }
+  LewisAndShort.save(result);
+} else {
+  LewisAndShort.save(LewisAndShort.createProcessed());
+}
+
 const runTime = Math.round(performance.now() - startTime);
 console.log(`Runtime: ${runTime} ms.`);
