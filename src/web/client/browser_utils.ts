0ea8792bdb6f89c9b8279ae0b833f2c3fdc34efb
Nitin Prasad <nkprasa@gmail.com>
2023-05-09 10:26:27 -0500

    Update browser utils to handle query parameters

--- src/web/client/browser_utils.ts
+++ src/web/client/browser_utils.ts
@@ -1,14 +1,34 @@
 const ERROR_MESSAGE = "Error: please try again later.";
 
-/**
- * @returns The URL decoded value of the current hash path value.
- */
-export function getHash(): string {
+function decodedHash(): string {
   const hash = window.location.hash;
   if (hash.length === 0) {
     return "";
   }
   return decodeURI(hash.substring(1));
+}
+
+/**
+ * @returns The URL decoded value of the current hash path value.
+ */
+export function getHash(): string {
+  return decodedHash().split("?")[0];
+}
+
+/**
+ * @returns A map of parameters encoded in the URL.
+ */
+export function getUrlParams(): Map<string, string> {
+  const parts = decodedHash().split("?").slice(1);
+  if (parts.length === 0) {
+    return new Map<string, string>();
+  }
+  const result = new Map<string, string>();
+  for (const part of parts) {
+    const chunks = part.split("=");
+    result.set(chunks[0], chunks.slice(1).join("="));
+  }
+  return result;
 }
 
 export async function backendCall(api: string, options?: any): Promise<string> {
