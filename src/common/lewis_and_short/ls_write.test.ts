8ca9a54c9a34b6b64b52d1eaa97c2f1099726dbe
Nitin Prasad <nkprasa@gmail.com>
2023-05-23 11:38:02 -0500

    Add utilities for searching and editing XML text

--- src/common/lewis_and_short/ls_write.test.ts
+++ src/common/lewis_and_short/ls_write.test.ts
@@ -1,7 +1,13 @@
 import { readFileSync, unlinkSync, writeFileSync } from "fs";
-import { LsRewriters, rewriteLs } from "./ls_write";
+import {
+  LsRewriters,
+  iterateFromNode,
+  removeTextNode,
+  rewriteLs,
+  searchTree,
+} from "./ls_write";
 import { CANABA, PALUS1 } from "./sample_entries";
-import { XmlNode } from "./xml_node";
+import { XmlChild, XmlNode } from "./xml_node";
 
 const IN_FILE = "ls_write.test.tmp.in.xml";
 
@@ -16,6 +22,10 @@ afterEach(() => {
     unlinkSync(IN_FILE);
   } catch (e) {}
 });
+
+function makeNode(children: XmlChild[]): XmlNode {
+  return new XmlNode("a", [], children);
+}
 
 describe("rewriters", () => {
   test("rewriteLs handles header correctly", async () => {
@@ -103,5 +113,120 @@ describe("rewriters", () => {
         `<box>${PALUS1}</box>`,
       ].join("\n")
     );
+  });
+});
+
+describe("iterateFromNode", () => {
+  const A = makeNode(["A"]);
+  const B = makeNode(["B", A, "Ba"]);
+  const C = makeNode(["C"]);
+  const D = makeNode([B, C]);
+
+  it("contains all text nodes in order", () => {
+    const result = iterateFromNode(D);
+
+    expect(result).toHaveLength(4);
+    const textNodes = result.map((d) => d.text);
+    expect(textNodes).toStrictEqual(["B", "A", "Ba", "C"]);
+  });
+
+  it("has correct parents", () => {
+    const result = iterateFromNode(D);
+
+    expect(result[0].parent).toBe(B);
+    expect(result[1].parent).toBe(A);
+    expect(result[2].parent).toBe(B);
+    expect(result[3].parent).toBe(C);
+  });
+
+  it("has correct indicies", () => {
+    const result = iterateFromNode(D);
+
+    expect(result[0].textIndex).toBe(0);
+    expect(result[1].textIndex).toBe(0);
+    expect(result[2].textIndex).toBe(2);
+    expect(result[3].textIndex).toBe(0);
+  });
+
+  it("has correct ancestors", () => {
+    const result = iterateFromNode(D);
+
+    expect(result[0].ancestors).toStrictEqual([D]);
+    expect(result[1].ancestors).toStrictEqual([D, B]);
+    expect(result[2].ancestors).toStrictEqual([D]);
+    expect(result[3].ancestors).toStrictEqual([D]);
+  });
+});
+
+describe("removeTextNode", () => {
+  it("handles removal of node with siblings", () => {
+    const A = makeNode(["A"]);
+    const B = makeNode(["B", A, "Ba"]);
+    const nodes = iterateFromNode(B);
+
+    removeTextNode(nodes[0]);
+
+    expect(B.children).toHaveLength(2);
+    expect(B.children[0]).toBe(A);
+    expect(B.children[1]).toBe("Ba");
+  });
+
+  it("handles removal of node without siblings", () => {
+    const A = makeNode(["A"]);
+    const B = makeNode(["B", A, "Ba"]);
+    const nodes = iterateFromNode(B);
+
+    removeTextNode(nodes[1]);
+
+    expect(B.children).toHaveLength(2);
+    expect(B.children[0]).toBe("B");
+    expect(B.children[1]).toBe("Ba");
+  });
+});
+
+describe("searchTree", () => {
+  const ha = makeNode([" ha"]);
+  const happ = makeNode([" sad", ha, "pp"]);
+  const y = makeNode(["y "]);
+  const happy = makeNode([" happy "]);
+  const sad = makeNode(["sad"]);
+  const root = makeNode([happ, y, sad, happy]);
+
+  test("matches have expected contents", () => {
+    const result = searchTree(root, ["happy"]);
+
+    expect(result.matches).toHaveLength(2);
+    const first = result.matches[0].chunks;
+    const second = result.matches[1].chunks;
+
+    expect(first).toHaveLength(3);
+    expect(first[0].data.parent).toBe(ha);
+    expect(first[0].match).toBe("ha");
+    expect(first[0].startIdx).toBe(1);
+    expect(first[0].endIdx).toBe(3);
+    expect(first[1].data.parent).toBe(happ);
+    expect(first[1].match).toBe("pp");
+    expect(first[1].startIdx).toBe(0);
+    expect(first[1].endIdx).toBe(2);
+    expect(first[2].data.parent).toBe(y);
+    expect(first[2].match).toBe("y");
+    expect(first[2].startIdx).toBe(0);
+    expect(first[2].endIdx).toBe(1);
+
+    expect(second).toHaveLength(1);
+    expect(second[0].data.parent).toBe(happy);
+    expect(second[0].match).toBe("happy");
+    expect(second[0].startIdx).toBe(1);
+    expect(second[0].endIdx).toBe(6);
+  });
+
+  it("finds expected matches for multiple targets", () => {
+    const result = searchTree(root, ["happy", "sad"]);
+
+    expect(result.matches).toHaveLength(4);
+    expect(result.matches[0].target).toBe("happy");
+    expect(result.matches[1].target).toBe("happy");
+    expect(result.matches[2].target).toBe("sad");
+    expect(result.matches[3].target).toBe("sad");
   });
 });
--- src/common/lewis_and_short/ls_write.ts
+++ src/common/lewis_and_short/ls_write.ts
@@ -1,5 +1,6 @@
 import { createWriteStream, readFileSync, renameSync } from "fs";
 import { parseEntries, XmlNode } from "./xml_node";
+import { assert } from "@/common/assert";
 
 /**
  * Rewrites the contents of an XML file at the specified path by applying
@@ -67,4 +68,172 @@ export namespace LsRewriters {
       return "\n" + transformer(original).toString();
     });
   }
+}
+
+/** A node of text within an XML tree. */
+export interface TextNodeData {
+  /** The content of the text node. */
+  text: string;
+  /** The parent node containing this text. */
+  parent: XmlNode;
+  /** The index of the text in the parent node's children. */
+  textIndex: number;
+  /** All ancestors of the parent node. Closer ancestors should be later in the list. */
+  ancestors: XmlNode[];
+}
+
+/**
+ * Returns all nodes containing text in the tree rooted by the input node.
+ *
+ * @param root The root node of the tree to search from.
+ * @param ancestors Ancestors of the root node in the overall tree, if any.
+ *                  Closer ancestors should be later in the list.
+ *
+ * @returns Data for all text nodes, in sequence.
+ */
+export function iterateFromNode(
+  root: XmlNode,
+  ancestors: XmlNode[] = []
+): TextNodeData[] {
+  let results: TextNodeData[] = [];
+  root.children.forEach((child, i) => {
+    if (typeof child === "string") {
+      results.push({
+        text: child,
+        parent: root,
+        textIndex: i,
+        ancestors: ancestors.map((x) => x),
+      });
+    } else {
+      results = results.concat(
+        iterateFromNode(child, ancestors.concat([root]))
+      );
+    }
+  });
+  return results;
+}
+
+/** Removes the given text node from the tree. */
+export function removeTextNode(data: TextNodeData) {
+  data.parent.children.splice(data.textIndex, 1);
+  const ancestors = [data.parent].concat(data.ancestors.reverse());
+  for (let i = 0; i < ancestors.length - 1; i++) {
+    const ancestor = ancestors[i];
+    if (ancestor.children.length > 0) {
+      break;
+    }
+    const nextParent = ancestors[i + 1];
+    const removeIndex = nextParent.children.indexOf(ancestor);
+    assert(removeIndex > -1);
+    nextParent.children.splice(removeIndex, 1);
+  }
+}
+
+/**
+ * Represents a text node in an XML tree that is a part of a match of
+ * a string search of the entire tree, potentially spanning multiple nodes.
+ */
+export interface MatchedChunk {
+  /** The text node matched. */
+  data: TextNodeData;
+  /** The portion of the target string that was matched. */
+  match: string;
+  /** The start index (inclusive) of the match within the text node. */
+  startIdx: number;
+  /** The end index (exclusive) of the match within the text node. */
+  endIdx: number;
+}
+
+/** A single match for one target. */
+export interface TargetMatch {
+  /** The target string that was matched. */
+  target: string;
+  /** The chunks making up the match, in order. */
+  chunks: MatchedChunk[];
+}
+
+/** The result of a cross node XML text search. */
+export interface MatchResult {
+  allTextNodes: TextNodeData[];
+  matches: TargetMatch[];
+}
+
+/**
+ * Searches the tree with the given root for the given targets.
+ *
+ * @param root The root of the tree to search.
+ * @param targets The strings to search for.
+ *
+ * @returns The matches for the given targets.
+ */
+export function searchTree(root: XmlNode, targets: string[]): MatchResult {
+  const textNodes = iterateFromNode(root, []);
+  const chunks = textNodes.map((data) => data.text);
+  const matches: TargetMatch[] = [];
+
+  const starts = [0];
+  for (let i = 0; i < chunks.length; i++) {
+    starts.push(starts[i] + chunks[i].length);
+  }
+  const allText = chunks.join("");
+
+  for (const target of targets) {
+    const matchStarts: number[] = [];
+    let startIndex = 0;
+    while (true) {
+      const matchStart = allText.indexOf(target, startIndex);
+      if (matchStart === -1) {
+        break;
+      }
+      matchStarts.push(matchStart);
+      startIndex = matchStart + target.length;
+    }
+    for (const start of matchStarts) {
+      let startChunk = -1;
+      let startOffset = undefined;
+      for (let i = 0; i < starts.length - 1; i++) {
+        if (starts[i] <= start && start < starts[i + 1]) {
+          startChunk = i;
+          startOffset = start - starts[i];
+          break;
+        }
+      }
+      const end = start + target.length - 1;
+      let endChunk = -1;
+      let endOffset = undefined;
+      for (let i = 0; i < starts.length - 1; i++) {
+        if (starts[i] <= end && end < starts[i + 1]) {
+          endChunk = i;
+          endOffset = end - starts[i] + 1;
+          break;
+        }
+      }
+
+      assert(startChunk > -1, "Expected to find a start chunk");
+      assert(endChunk > -1, "Expected to find an end chunk");
+      assert(
+        endChunk >= startChunk,
+        "Expected end chunk to be after the start chunk"
+      );
+      assert(startOffset !== undefined, "Expected to find a start offset");
+      assert(endOffset !== undefined, "Expected to find an end offset");
+
+      const matchNodes = textNodes.slice(startChunk, endChunk + 1);
+      const matchChunks: MatchedChunk[] = [];
+      const n = matchNodes.length;
+      for (let i = 0; i < n; i++) {
+        const nodeText = matchNodes[i].text;
+        const j = i === 0 ? startOffset! : 0;
+        const k = i === n - 1 ? endOffset! : nodeText.length;
+        matchChunks.push({
+          data: matchNodes[i],
+          match: nodeText.substring(j, k),
+          startIdx: j,
+          endIdx: k,
+        });
+      }
+      matches.push({ target: target, chunks: matchChunks });
+    }
+  }
+  return { allTextNodes: textNodes, matches: matches };
 }
