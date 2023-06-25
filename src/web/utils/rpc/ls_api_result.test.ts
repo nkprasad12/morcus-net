243c28da96cf192a7c69de3423f07204b807e46a
Nitin Prasad <nkprasa@gmail.com>
2023-06-25 16:19:36 -0500

    Add types for LS outline API

--- src/web/utils/rpc/ls_api_result.test.ts
+++ src/web/utils/rpc/ls_api_result.test.ts
@@ -1,14 +1,51 @@
 import { XmlNode } from "@/common/lewis_and_short/xml_node";
 import { LsResult } from "./ls_api_result";
 
-describe("LsResult type", () => {
+describe("Ls API types", () => {
   test("isMatch returns false on raw node", () => {
     const input = new XmlNode("span");
     expect(LsResult.isMatch(input)).toBe(false);
   });
 
-  test("isMatch returns true on good input", () => {
+  test("LsResult.isMatch returns true on no outline", () => {
     const input = new XmlNode("span");
     expect(LsResult.isMatch({ entry: input })).toBe(true);
+  });
+
+  test("LsResult.isMatch returns false without main section", () => {
+    const input = new XmlNode("span");
+    expect(LsResult.isMatch({ entry: input, outline: { senses: [] } })).toBe(
+      false
+    );
+  });
+
+  test("LsResult.isMatch returns true with bad outline", () => {
+    const input = new XmlNode("span");
+    expect(
+      LsResult.isMatch({
+        entry: input,
+        outline: {
+          mainSection: { text: "foo", level: 1, ordinal: "a", section: "foo" },
+          senses: [],
+        },
+      })
+    ).toBe(false);
+  });
+
+  test("LsResult.isMatch returns true with good outline", () => {
+    const input = new XmlNode("span");
+    const result: LsResult = {
+      entry: input,
+      outline: {
+        mainSection: {
+          text: "foo",
+          level: 1,
+          ordinal: "a",
+          sectionId: "foo",
+        },
+        senses: [],
+      },
+    };
+    expect(LsResult.isMatch(result)).toBe(true);
   });
 });
