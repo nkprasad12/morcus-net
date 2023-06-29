be0b17967268229be619fa72e808ee71ca555031
Nitin Prasad <nkprasa@gmail.com>
2023-06-28 23:29:26 -0500

    Add macron / breve sensitivity in searches

---
+++ src/common/character_utils.test.ts
@@ -0,0 +1,18 @@
+import { Vowels } from "./character_utils";
+
+describe("Vowels getLength", () => {
+  it("returns unspecified on other", () => {
+    expect(Vowels.getLength("a")).toBe("Ambiguous");
+    expect(Vowels.getLength("á")).toBe("Ambiguous");
+  });
+
+  it("returns Long on macron", () => {
+    expect(Vowels.getLength("ō")).toBe("Long");
+    expect(Vowels.getLength("Ē")).toBe("Long");
+  });
+
+  it("returns short on breve", () => {
+    expect(Vowels.getLength("ў")).toBe("Short");
+    expect(Vowels.getLength("Ă")).toBe("Short");
+  });
+});
