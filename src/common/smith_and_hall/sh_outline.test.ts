127163f64a2236c49c5313fd38244d19efe3f3a4
Nitin Prasad <nkprasa@gmail.com>
2023-08-18 11:17:04 -0500

    Add basic test for sh_outline

---
+++ src/common/smith_and_hall/sh_outline.test.ts
@@ -0,0 +1,31 @@
+import { ShEntry } from "@/common/smith_and_hall/sh_entry";
+import { getOutline } from "@/common/smith_and_hall/sh_outline";
+
+describe("getOutline", () => {
+  it("pipes through correct data", () => {
+    const input: ShEntry = {
+      keys: ["Hello", "Hi"],
+      blurb: "Greetings",
+      senses: [
+        { level: 1, bullet: "I.", text: "Formal" },
+        { level: 1, bullet: "II.", text: "Informal" },
+      ],
+    };
+
+    const result = getOutline(input, 57);
+
+    expect(result).toEqual({
+      mainOrth: "Hello",
+      mainSection: {
+        level: 0,
+        ordinal: "0",
+        text: "Greetings",
+        sectionId: "sh57",
+      },
+      senses: [
+        { level: 1, ordinal: "I.", text: "Formal", sectionId: "sh57.0" },
+        { level: 1, ordinal: "II.", text: "Informal", sectionId: "sh57.1" },
+      ],
+    });
+  });
+});
