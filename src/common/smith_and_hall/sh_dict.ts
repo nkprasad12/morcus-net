7fd7e3286d597bb57945bd04629c28f04faa72ec
Nitin Prasad <nkprasa@gmail.com>
2023-08-17 23:33:59 -0500

    Inline function for clarity

--- src/common/smith_and_hall/sh_dict.ts
+++ src/common/smith_and_hall/sh_dict.ts
@@ -1,29 +1,21 @@
 import { checkPresent } from "@/common/assert";
-import { EntryResult } from "@/common/dictionaries/dict_result";
 import { SqlDict } from "@/common/dictionaries/dict_storage";
 import { LatinDict } from "@/common/dictionaries/latin_dicts";
 import { displayShEntry } from "@/common/smith_and_hall/sh_display";
 import { getOutline } from "@/common/smith_and_hall/sh_outline";
-import { ShEntry } from "@/common/smith_and_hall/sh_process";
-
-function toGenericDict(processed: ShEntry[]): EntryResult[] {
-  return processed.map((article, i) => {
-    return {
-      entry: displayShEntry(article, i),
-      outline: getOutline(article, i),
-    };
-  });
-}
 
 export class SmithAndHall extends SqlDict {
   constructor(dbPath: string = checkPresent(process.env.SH_PROCESSED_PATH)) {
     super(
       dbPath,
       LatinDict.SmithAndHall,
-      (entryStrings) => {
-        const processedEntries = entryStrings.map((x) => JSON.parse(x));
-        return toGenericDict(processedEntries);
-      },
+      (entryStrings) =>
+        entryStrings
+          .map((x) => JSON.parse(x))
+          .map((article, i) => ({
+            entry: displayShEntry(article, i),
+            outline: getOutline(article, i),
+          })),
       (input) => input.split("@")
     );
   }
