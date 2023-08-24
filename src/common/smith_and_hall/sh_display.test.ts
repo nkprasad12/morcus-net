d7b5e166b6636276381cf74ecea604e4fdb1319c
Nitin Prasad <nkprasa@gmail.com>
2023-08-24 00:39:01 -0500

    Add basic tests for link resolution

--- src/common/smith_and_hall/sh_display.test.ts
+++ src/common/smith_and_hall/sh_display.test.ts
@@ -1,4 +1,7 @@
-import { displayShEntry } from "@/common/smith_and_hall/sh_display";
+import {
+  ShLinkResolver,
+  displayShEntry,
+} from "@/common/smith_and_hall/sh_display";
 import { ShEntry } from "@/common/smith_and_hall/sh_entry";
 
 const TEST_ENTRY: ShEntry = {
@@ -6,23 +9,164 @@ const TEST_ENTRY: ShEntry = {
   blurb: "<b>Greetings</b>",
   senses: [
     { level: 1, bullet: "I", text: "'lo" },
-    { level: 2, bullet: "1", text: "heeelo <f>test</f>" },
-    { level: 1, bullet: "II", text: "suup" },
+    { level: 2, bullet: "1", text: "heeelo" },
+    { level: 1, bullet: "II", text: "<sc>suup</sc>" },
   ],
 };
 
+const ENTRY_WITH_LINKS: ShEntry = {
+  keys: ["Hi"],
+  blurb: "<f>GREETINGS</f>",
+  senses: [{ level: 1, bullet: "I", text: "<f>LO</f>" }],
+};
+
+function spoofEntries(input: string[][]): ShEntry[] {
+  return input.map((keys) => ({ keys: keys, blurb: "", senses: [] }));
+}
+
+function verifyLinkMarkup(
+  keys: string[][],
+  link: string,
+  to: string,
+  text?: string
+) {
+  const entries = spoofEntries(keys);
+  const result = displayShEntry(
+    { keys: ["Hi"], blurb: `<f>${link}</f>`, senses: [] },
+    57,
+    new ShLinkResolver(entries)
+  );
+  expect(result.toString()).toEqual(
+    [
+      `<div>`,
+      `<div id="sh57"><span class="lsSenseBullet" senseid="sh57"> ‣ </span> `,
+      `<span class="dLink" to="${to}" text="${
+        text || link.toLowerCase()
+      }"></span></div>`,
+      `<div></div>`,
+      `</div>`,
+    ].join("")
+  );
+}
+
 describe("displayShEntry", () => {
   it("displays nested senses correctly", () => {
-    const result = displayShEntry(TEST_ENTRY, 57);
+    const result = displayShEntry(TEST_ENTRY, 57, new ShLinkResolver([]));
     expect(result.toString()).toEqual(
       [
         `<div>`,
         `<div id="sh57"><span class="lsSenseBullet" senseid="sh57"> ‣ </span> <span class="lsOrth">Greetings</span></div>`,
         `<ol class="lsTopSense"><li id="sh57.0"><span class="lsSenseBullet" senseid="sh57.0"> I </span>'lo</li>`,
-        `<ol><li id="sh57.1"><span class="lsSenseBullet" senseid="sh57.1"> 1 </span>heeelo <span class="dLink">test</span></li></ol>`,
-        `<li id="sh57.2"><span class="lsSenseBullet" senseid="sh57.2"> II </span>suup</li></ol>`,
+        `<ol><li id="sh57.1"><span class="lsSenseBullet" senseid="sh57.1"> 1 </span>heeelo</li></ol>`,
+        `<li id="sh57.2"><span class="lsSenseBullet" senseid="sh57.2"> II </span><i>suup</i></li></ol>`,
         `</div>`,
       ].join("")
     );
+  });
+
+  it("displays unknown links correctly", () => {
+    const result = displayShEntry(ENTRY_WITH_LINKS, 57, new ShLinkResolver([]));
+    expect(result.toString()).toEqual(
+      [
+        `<div>`,
+        `<div id="sh57"><span class="lsSenseBullet" senseid="sh57"> ‣ </span> <span>GREETINGS</span></div>`,
+        `<ol class="lsTopSense"><li id="sh57.0">`,
+        `<span class="lsSenseBullet" senseid="sh57.0"> I </span><span>LO</span>`,
+        `</li></ol>`,
+        `</div>`,
+      ].join("")
+    );
+  });
+
+  it("displays known links correctly in blurb and sense", () => {
+    const keys = [["greetings"], ["lo"]];
+    const entries = spoofEntries(keys);
+    const result = displayShEntry(
+      ENTRY_WITH_LINKS,
+      57,
+      new ShLinkResolver(entries)
+    );
+    expect(result.toString()).toEqual(
+      [
+        `<div>`,
+        `<div id="sh57"><span class="lsSenseBullet" senseid="sh57"> ‣ </span> <span class="dLink" to="greetings" text="greetings"></span></div>`,
+        `<ol class="lsTopSense"><li id="sh57.0">`,
+        `<span class="lsSenseBullet" senseid="sh57.0"> I </span><span class="dLink" to="lo" text="lo"></span>`,
+        `</li></ol>`,
+        `</div>`,
+      ].join("")
+    );
+  });
+
+  it("handles <sc> edge case", () => {
+    const keys = [["greetings"], ["lo"]];
+    verifyLinkMarkup(keys, "<sc>LO</sc>", "lo", "lo");
+  });
+
+  it("removes leading to", () => {
+    const keys = [["go"]];
+    verifyLinkMarkup(keys, "TO GO", "go");
+  });
+
+  it("removes paren text with TO", () => {
+    const keys = [["go"]];
+    verifyLinkMarkup(keys, "TO GO (BLAH)", "go");
+  });
+
+  it("resolved keys with dashes", () => {
+    const keys = [["under-go"]];
+    verifyLinkMarkup(keys, "TO UNDERGO", "under-go");
+  });
+
+  it("resolves link edge cases", () => {
+    const keys = [["cann"]];
+    verifyLinkMarkup(keys, "CANNOT", "can");
+  });
+
+  it("resolves simple -ing edge cases", () => {
+    const keys = [["front"]];
+    verifyLinkMarkup(keys, "FRONTING", "front");
+  });
+
+  it("resolves e -ing edge cases", () => {
+    const keys = [["continue"]];
+    verifyLinkMarkup(keys, "CONTINUING", "continue");
+  });
+
+  it("resolves replaced suffix edge cases", () => {
+    const keys = [["ostracize"]];
+    verifyLinkMarkup(keys, "TO OSTRACISE", "ostracize");
+  });
+
+  it("attempts to remove plural s only if needed", () => {
+    const keys = [["flag"], ["swims", "swim"]];
+    verifyLinkMarkup(keys, "FLAGS", "flag");
+    verifyLinkMarkup(keys, "SWIMS", "swims");
+  });
+
+  it("resolves to shortest containing entry", () => {
+    const keys = [["spanner", "spanning"]];
+    verifyLinkMarkup(keys, "SPAN", "spanner");
+  });
+
+  it("tries to remove period", () => {
+    const keys = [["spanner"]];
+    verifyLinkMarkup(keys, "SPAN.", "spanner");
+  });
+
+  it("ignores after comma", () => {
+    const keys = [["spanner"]];
+    verifyLinkMarkup(keys, "SPAN, ABC", "spanner");
+  });
+
+  it("removes dashes but only if needed", () => {
+    const keys = [["thereupon"], ["easygoing", "easy-going"]];
+    verifyLinkMarkup(keys, "THERE-UPON", "thereupon");
+    verifyLinkMarkup(keys, "EASY-GOING", "easy-going");
+  });
+
+  it("attempts to remove final LY", () => {
+    const keys = [["quick"]];
+    verifyLinkMarkup(keys, "QUICKLY", "quick");
   });
 });
--- src/common/smith_and_hall/sh_display.ts
+++ src/common/smith_and_hall/sh_display.ts
@@ -1,37 +1,260 @@
 import { ShEntry, ShSense } from "@/common/smith_and_hall/sh_entry";
 import { XmlChild, XmlNode } from "@/common/xml_node";
 import { parseXmlStrings } from "../xml_utils";
+import { assertEqual } from "@/common/assert";
+import { LINK_EDGE_CASES, SH_SKIPS } from "@/common/smith_and_hall/sh_links";
 
-function markupText(senseRoot: XmlNode): XmlNode {
+const FILLER_WORDS = new Set<string>([
+  "OF",
+  "IN",
+  "BE",
+  "UP",
+  "ON",
+  "WITH",
+  "BY",
+  "TO",
+  "FOR",
+  "AT",
+  "THE",
+  "OVER",
+  "OFF",
+  "ONESELF",
+]);
+
+const SUFFIX_REPLACEMENTS = [
+  ["ISE", "IZE"],
+  ["IES", "Y"],
+];
+
+interface ShKeyData {
+  normalized: string;
+  original: string;
+}
+
+function removeParenText(input: string): string {
+  let result = "";
+  let inParen = false;
+  for (const c of input) {
+    if (c === "(" || c === "[") {
+      inParen = true;
+    }
+    if (c === ")" || c === "]") {
+      inParen = false;
+    }
+    if (!inParen) {
+      result += c;
+    }
+  }
+  return result.replaceAll("  ", " ").trim();
+}
+
+export class ShLinkResolver {
+  private readonly keysByStart: Map<string, ShKeyData[]> = new Map();
+
+  constructor(readonly entries: ShEntry[]) {
+    entries.forEach((entry) => {
+      entry.keys.forEach((key) => {
+        const keyLower = key.toLowerCase();
+        if (!this.keysByStart.has(keyLower[0])) {
+          this.keysByStart.set(keyLower[0], []);
+        }
+        this.keysByStart
+          .get(keyLower[0])!
+          .push({ normalized: keyLower, original: key });
+        const dashIndex = key.indexOf("-");
+        if (dashIndex > 0) {
+          this.keysByStart
+            .get(keyLower[0])!
+            .push({ normalized: keyLower.replaceAll("-", ""), original: key });
+        }
+      });
+    });
+  }
+
+  private resolveHelper(originalTerm: string): string | undefined {
+    const edgeCaseResult = LINK_EDGE_CASES.get(originalTerm);
+    if (edgeCaseResult !== undefined) {
+      return edgeCaseResult;
+    }
+    let term = originalTerm;
+    if (term.startsWith("TO ")) {
+      term = term.substring(3);
+    }
+    const ingRemoved = term.endsWith("ING");
+    if (ingRemoved) {
+      term = term.slice(0, -3);
+    }
+    const termLower = term.toLowerCase();
+
+    const sublist = this.keysByStart.get(termLower[0]) || [];
+    const candidates = sublist.filter(
+      (key) =>
+        key.normalized.startsWith(termLower) ||
+        termLower.startsWith(key.normalized)
+    );
+    if (candidates.length === 0) {
+      for (const [end, newEnd] of SUFFIX_REPLACEMENTS) {
+        if (term.endsWith(end)) {
+          return this.resolve(term.slice(0, -end.length) + newEnd);
+        }
+      }
+      return undefined;
+    }
+    for (const candidate of candidates) {
+      const target = termLower + (ingRemoved ? "ing" : "");
+      if (candidate.normalized === target) {
+        return candidate.original;
+      }
+    }
+    if (ingRemoved) {
+      for (const candidate of candidates) {
+        const target = termLower + "e";
+        if (candidate.normalized === target) {
+          return candidate.original;
+        }
+      }
+    }
+    if (termLower.endsWith("s")) {
+      for (const candidate of candidates) {
+        const target = termLower.slice(0, -1);
+        if (candidate.normalized === target) {
+          return candidate.original;
+        }
+      }
+    }
+
+    const longer = candidates.filter((candidate) =>
+      candidate.normalized.startsWith(termLower)
+    );
+    if (longer.length === 0) {
+      return undefined;
+    }
+    const shortest = longer.reduce((shortest, current) =>
+      current.normalized.length < shortest.normalized.length
+        ? current
+        : shortest
+    );
+    return shortest.original;
+  }
+
+  resolve(term: string): string | undefined {
+    let result = this.resolveHelper(term);
+    if (result !== undefined) {
+      return result;
+    }
+    let simplified = removeParenText(term.replaceAll("-*", "-").split(",")[0]);
+    if (simplified.endsWith(".")) {
+      simplified = simplified.slice(0, -1);
+    }
+    result = this.resolveHelper(simplified);
+    if (result !== undefined) {
+      return result;
+    }
+    if (simplified.includes("-")) {
+      simplified = simplified.replaceAll("-", "");
+      result = this.resolveHelper(simplified);
+      if (result !== undefined) {
+        return result;
+      }
+    }
+    if (simplified.endsWith("LY")) {
+      result = this.resolveHelper(simplified.slice(0, -2));
+      if (result !== undefined) {
+        return result;
+      }
+    }
+    simplified = term
+      .split(" ")
+      .filter(
+        (word) =>
+          !FILLER_WORDS.has(word) &&
+          !word.startsWith("(") &&
+          !word.startsWith("[")
+      )
+      .join(" ");
+    result = this.resolveHelper(simplified);
+    if (result !== undefined) {
+      return result;
+    }
+    return undefined;
+  }
+}
+
+function markupText(senseRoot: XmlNode, resolver: ShLinkResolver): XmlNode {
   let name = senseRoot.name;
-  if (["b", "f"].includes(senseRoot.name)) {
+  if (["b"].includes(senseRoot.name)) {
     name = "span";
+  }
+  if (["f"].includes(senseRoot.name)) {
+    name = "span";
+  }
+  if (["sc"].includes(senseRoot.name)) {
+    name = "i";
   }
   const attrs: [string, string][] = [];
   if (senseRoot.name === "b") {
     attrs.push(["class", "lsOrth"]);
   }
   if (senseRoot.name === "f") {
-    attrs.push(["class", "dLink"]);
+    assertEqual(senseRoot.children.length, 1);
+    const child = senseRoot.children[0];
+    let linkText = "";
+    if (typeof child !== "string") {
+      assertEqual(child.children.length, 1);
+      linkText = XmlNode.assertIsString(child.children[0]);
+    } else {
+      linkText = child;
+    }
+    if (
+      !linkText.startsWith("i. e.") &&
+      !linkText.startsWith("i.e.") &&
+      !SH_SKIPS.has(linkText)
+    ) {
+      const linkTo = resolver.resolve(linkText);
+      if (linkTo === undefined) {
+        // If trying to debug edge cases, uncomment this so
+        // anything unresolved will throw.
+        //
+        // if (/[a-z]/.test(linkText[0])) {
+        //   return new XmlNode("span", [], [linkText]);
+        // }
+        // throw linkText;
+        return new XmlNode("span", [], [linkText]);
+      }
+      attrs.push(["class", "dLink"]);
+      attrs.push(["to", linkTo]);
+      attrs.push(["text", linkText.toLowerCase()]);
+      return new XmlNode(name, attrs, []);
+    }
   }
   const children = senseRoot.children.map((child) =>
-    typeof child === "string" ? child : markupText(child)
+    typeof child === "string" ? child : markupText(child, resolver)
   );
   return new XmlNode(name, attrs, children);
 }
 
-function getMarkedUpText(senses: string[]): XmlChild[][] {
+function getMarkedUpText(
+  senses: string[],
+  resolver: ShLinkResolver
+): XmlChild[][] {
   return parseXmlStrings(senses.map((sense) => `<div>${sense}</div>`)).map(
-    (parsedXml) => markupText(parsedXml).children
+    (parsedXml) => markupText(parsedXml, resolver).children
   );
 }
 
-function formatSenseList(senses: ShSense[], entryId: number): XmlNode {
+function formatSenseList(
+  senses: ShSense[],
+  entryId: number,
+  resolver: ShLinkResolver
+): XmlNode {
   if (senses.length === 0) {
     return new XmlNode("div");
   }
   const stack: XmlNode[] = [];
-  const parsedText = getMarkedUpText(senses.map((s) => s.text));
+  const parsedText = getMarkedUpText(
+    senses.map((s) => s.text),
+    resolver
+  );
 
   for (let i = 0; i < senses.length; i++) {
     const sense = senses[i];
@@ -71,7 +294,11 @@ function formatSenseList(senses: ShSense[], entryId: number): XmlNode {
   return stack[0];
 }
 
-export function displayShEntry(entry: ShEntry, id: number): XmlNode {
+export function displayShEntry(
+  entry: ShEntry,
+  id: number,
+  resolver: ShLinkResolver
+): XmlNode {
   const mainBlurbButton: XmlChild[] = [
     new XmlNode(
       "span",
@@ -86,8 +313,12 @@ export function displayShEntry(entry: ShEntry, id: number): XmlNode {
   const blurbText = new XmlNode(
     "div",
     [["id", `sh${id}`]],
-    mainBlurbButton.concat(...getMarkedUpText([entry.blurb]))
+    mainBlurbButton.concat(...getMarkedUpText([entry.blurb], resolver))
   );
 
-  return new XmlNode("div", [], [blurbText, formatSenseList(entry.senses, id)]);
+  return new XmlNode(
+    "div",
+    [],
+    [blurbText, formatSenseList(entry.senses, id, resolver)]
+  );
 }
