274ff986e44a656dfa093a5dc2363259b4747a34
Nitin Prasad <nkprasa@gmail.com>
2023-11-06 10:31:11 -0600

    Add script to write Latin inflections db

---
+++ src/scripts/latin_inflections.ts
@@ -0,0 +1,8 @@
+import { makeMorpheusDb } from "@/common/lexica/latin_words";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+const startTime = performance.now();
+makeMorpheusDb();
+const runTime = Math.round(performance.now() - startTime);
+console.log(`Latin inflection table creation runtime: ${runTime} ms.`);
