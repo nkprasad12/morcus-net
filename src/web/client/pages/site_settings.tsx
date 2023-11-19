7d09af8590ac16a26de2a76b54d1f6e4f0626513
Nitin Prasad <nkprasa@gmail.com>
2023-11-18 22:38:27 -0600

    Refactor site settings

--- src/web/client/pages/site_settings.tsx
+++ src/web/client/pages/site_settings.tsx
@@ -4,36 +4,53 @@ import Typography from "@mui/material/Typography";
 
 import React, { useContext } from "react";
 import { Solarized } from "@/web/client/colors";
-import { GlobalSettingsContext } from "@/web/client/components/global_flags";
+import {
+  GlobalSettings,
+  GlobalSettingsContext,
+} from "@/web/client/components/global_flags";
+import { checkSatisfies } from "@/common/assert";
+import { isBoolean } from "@/web/utils/rpc/parsing";
+
+function GlobalSettingsCheckbox(props: {
+  label: string;
+  settingKey: keyof GlobalSettings;
+}) {
+  const globalSettings = useContext(GlobalSettingsContext);
+  const settingValue = checkSatisfies(
+    globalSettings.data[props.settingKey],
+    isBoolean
+  );
+  const enabled = settingValue === true;
+
+  return (
+    <div>
+      <input
+        id={props.label}
+        type="checkbox"
+        checked={enabled}
+        onChange={(e) => {
+          const newSettings = { ...globalSettings.data };
+          // @ts-ignore - we checked above that this is a boolean field.
+          newSettings[props.settingKey] = e.currentTarget.checked;
+          globalSettings.setData(newSettings);
+        }}
+      />
+      <label htmlFor={props.label} style={{ paddingLeft: 8 }}>
+        {props.label}
+      </label>
+    </div>
+  );
+}
 
 export function SiteSettings() {
-  const globalSettings = useContext(GlobalSettingsContext);
-
-  const experimentalMode = globalSettings.data.experimentalMode === true;
-
   return (
     <Container maxWidth="lg">
       <Box sx={{ padding: 3 }}>
         <Typography component={"div"} color={Solarized.base00}>
-          <div>
-            <input
-              id="enableExperimentalToggle"
-              type="checkbox"
-              checked={experimentalMode}
-              onChange={(e) =>
-                globalSettings.setData({
-                  ...globalSettings.data,
-                  experimentalMode: e.currentTarget.checked,
-                })
-              }
-            />
-            <label
-              htmlFor="enableExperimentalToggle"
-              style={{ paddingLeft: 8 }}
-            >
-              Enable Experimental Features
-            </label>
-          </div>
+          <GlobalSettingsCheckbox
+            label="Enable Experimental Settings"
+            settingKey="experimentalMode"
+          />
         </Typography>
       </Box>
     </Container>
