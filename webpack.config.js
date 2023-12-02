9375c3b52a60eae956e97c3603261f95ec05f293
Nitin Prasad <nkprasa@gmail.com>
2023-12-02 11:32:20 -0600

    Update prod config to minify Root

--- webpack.config.js
+++ webpack.config.js
@@ -46,12 +46,12 @@ module.exports = (env) => {
     minimize: shouldMinimize,
     minimizer: [
       new TerserPlugin({
-        include: "vendorBundle",
+        include: ["vendorBundle", "Root"],
       }),
     ],
     splitChunks: {
       chunks: "all",
-      minSize: 250000,
+      minSize: 300000,
       maxSize: 500000,
       cacheGroups: {
         vendorBundle: {
