43150e7cdb63b31fe32f7cf832731871c74a2d70
Nitin Prasad <nkprasa@gmail.com>
2023-12-15 21:27:17 -0600

    Add placeholder test for analyzeLatin

---
+++ src/morceus/analyzer.test.ts
@@ -0,0 +1,8 @@
+import { analyzeLatin } from "@/morceus/analyzer";
+
+describe("analyzeLatin", () => {
+  it("returns expected on single word", () => {
+    // Just a placeholder test/
+    expect(analyzeLatin("hello")).toEqual([]);
+  });
+});
