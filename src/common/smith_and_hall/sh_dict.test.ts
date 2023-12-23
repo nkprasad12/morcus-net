929bb59ac8192b469ae459512ae991cddfae407a
Nitin Prasad <nkprasa@gmail.com>
2023-12-22 21:08:04 -0600

    Use fake morph dict for some tests

--- src/common/smith_and_hall/sh_dict.test.ts
+++ src/common/smith_and_hall/sh_dict.test.ts
@@ -1,14 +1,33 @@
 import { EntryResult } from "@/common/dictionaries/dict_result";
 import { SqlDict } from "@/common/dictionaries/dict_storage";
+import { makeMorpheusDb } from "@/common/lexica/latin_words";
+import { SAMPLE_MORPHEUS_OUTPUT } from "@/common/lexica/morpheus_testdata";
 import { SmithAndHall } from "@/common/smith_and_hall/sh_dict";
 import { ShEntry } from "@/common/smith_and_hall/sh_entry";
 import { shListToRaw } from "@/common/smith_and_hall/sh_process";
+import { cleanupSqlTableFiles } from "@/common/sql_test_helper";
 import { XmlNode } from "@/common/xml/xml_node";
-import fs from "fs";
+import fs, { unlinkSync, writeFileSync } from "fs";
 
 console.debug = jest.fn();
 
 const TEMP_FILE = "sh_dict.test.ts.tmp.txt";
+
+const MORPH_FILE = "sh_dict.test.ts.tmp.morph.txt";
+const INFL_DB_FILE = "sh_dict.test.ts.tmp.lat.db";
+
+beforeAll(() => {
+  process.env.LATIN_INFLECTION_DB = INFL_DB_FILE;
+  writeFileSync(MORPH_FILE, SAMPLE_MORPHEUS_OUTPUT);
+  makeMorpheusDb(MORPH_FILE, INFL_DB_FILE);
+});
+
+afterAll(() => {
+  try {
+    unlinkSync(MORPH_FILE);
+  } catch {}
+  cleanupSqlTableFiles(INFL_DB_FILE);
+});
 
 const SH_ENTRIES: ShEntry[] = [
   { keys: ["Hi", "Hello"], blurb: "Greetings", senses: [] },
