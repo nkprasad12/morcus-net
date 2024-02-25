eb4fcbed8abe212752d5a0e4ad119584023a405a
Nitin Prasad <nkprasa@gmail.com>
2024-02-25 02:06:04 -0600

    Fix ls_interactive issues

--- src/common/lewis_and_short/editor/ls_interactive.ts
+++ src/common/lewis_and_short/editor/ls_interactive.ts
@@ -3,9 +3,9 @@
 import { LS_PATH } from "@/common/lewis_and_short/ls_scripts";
 import { LsRewriters } from "@/common/lewis_and_short/ls_write";
 import { XmlNode } from "@/common/xml/xml_node";
-import http from "http";
-import bodyParser from "body-parser";
-import express, { Response } from "express";
+import fastify, { FastifyReply as Response } from "fastify";
+import path from "path";
+import fastifyStatic from "@fastify/static";
 import { checkPresent } from "@/common/assert";
 import { parseXmlStrings } from "@/common/xml/xml_utils";
 import { XmlNodeSerialization } from "@/common/xml/xml_node_serialization";
@@ -25,19 +25,20 @@ export async function editLs(
 
 export async function startInteractiveEditor() {
   const port = parseInt(checkPresent(process.env.PORT));
-  const app = express();
-  const server = http.createServer(app);
+  const app = fastify();
 
   let pendingResolve: undefined | ((root: XmlNode) => any) = undefined;
   let pendingResponse: undefined | Response;
   let editLsPromise: undefined | Promise<void>;
 
-  app.use(express.static("./genfiles_static"));
-  app.use(bodyParser.text());
+  await app.register(fastifyStatic, {
+    root: path.resolve("./genfiles_static"),
+  });
   app.post("/respond", (req, res) => {
     console.log("Get browser message");
     pendingResponse = res;
     if (pendingResolve !== undefined) {
+      // @ts-ignore
       pendingResolve(parseXmlStrings([req.body])[0]);
     }
     if (editLsPromise === undefined) {
@@ -57,19 +58,15 @@ export async function startInteractiveEditor() {
         console.log("Saved edited LS.");
         if (pendingResponse !== undefined) {
           pendingResponse.send("All done!");
-          server.close();
+          app.close();
         }
       });
     }
   });
 
-  await new Promise<void>((resolve) =>
-    server.listen(port, async () => {
-      console.log(
-        `Interactive editor running! Go to http://localhost:${port}/ls_editor_index.html`
-      );
-      resolve();
-    })
+  await app.listen({ port });
+  console.log(
+    `Interactive editor running! Go to http://localhost:${port}/ls_editor_index.html`
   );
 }
 
