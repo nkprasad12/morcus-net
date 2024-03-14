a4d7beaf1e9632f0a59dc369ddc78d20bdf3d2a4
Nitin Prasad <nkprasa@gmail.com>
2024-03-14 10:49:31 -0500

    Minor script edits

--- src/devops/update_containers.sh
+++ src/devops/update_containers.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 # Schedule this script via `crontab`
+# Run: `crontab -e`, then add:
+# * * * * * cd /root/morcus2 && ./update_container.sh &>> update_log.txt
+# @reboot cd /root/morcus2 && ./update_container.sh &>> update_log.txt
+
+echo -e "\n==== Checking for updates ===="
 docker compose pull
 docker compose up -d --remove-orphans
 yes | docker image prune
