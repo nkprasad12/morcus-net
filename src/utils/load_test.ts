3ec606e7403818703d60719fb6665492fdd09a0f
Nitin Prasad <nkprasa@gmail.com>
2024-03-17 23:49:48 -0500

    Add basic server load test

---
+++ src/utils/load_test.ts
@@ -0,0 +1,29 @@
+/* istanbul ignore file */
+
+import { CompletionsFusedApi, DictsFusedApi } from "@/web/api_routes";
+import { callApi } from "@/web/utils/rpc/client_rpc";
+import * as dotenv from "dotenv";
+import fetch from "node-fetch";
+
+dotenv.config();
+
+// @ts-ignore
+global.location = {
+  origin: `https://dev-hz.morcus.net`,
+};
+
+async function generateLoad() {
+  await fetch("https://dev-hz.morcus.net/Root.X3PSYSMZ.js");
+  await callApi(DictsFusedApi, { query: "habeo", dicts: ["L&S", "S&H"] });
+  await Promise.allSettled([
+    callApi(DictsFusedApi, { query: "canaba", dicts: ["L&S", "S&H"] }),
+    callApi(DictsFusedApi, { query: "unda", dicts: ["L&S", "S&H"] }),
+  ]);
+  callApi(DictsFusedApi, { query: "canem", dicts: ["L&S", "S&H"], mode: 1 });
+  callApi(CompletionsFusedApi, {
+    query: "c",
+    dicts: ["S&H", "L&S"],
+  });
+}
+
+setInterval(generateLoad, 1000);
