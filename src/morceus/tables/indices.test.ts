eef6d5d9dd0648f6f307cb4fe97fa8e489e08860
Nitin Prasad <nkprasa@gmail.com>
2024-03-23 23:59:12 -0500

    Add basic tests for nom end index

--- src/morceus/tables/indices.test.ts
+++ src/morceus/tables/indices.test.ts
@@ -1,4 +1,12 @@
-import { EndIndexRow } from "@/morceus/tables/indices";
+import {
+  EndIndexRow,
+  makeEndIndex,
+  type EndsResult,
+} from "@/morceus/tables/indices";
+
+const TESTDATA_DIR = "src/morceus/tables/lat/core/testdata/";
+const DEP_TEMPLATES = `${TESTDATA_DIR}dependencyTemplates`;
+const TARGET_TEMPLATES = `${TESTDATA_DIR}targetTemplates`;
 
 describe("EndIndexRow", () => {
   it("parses and stringifies correctly", () => {
@@ -9,5 +17,45 @@ describe("EndIndexRow", () => {
     expect(EndIndexRow.parse(EndIndexRow.stringify(original))).toEqual(
       original
     );
+  });
+});
+
+describe("makeEndIndex", () => {
+  it("Makes expected index for simple case", () => {
+    const result = makeEndIndex([TARGET_TEMPLATES], [DEP_TEMPLATES]);
+    expect(result).toStrictEqual<EndsResult>([
+      [
+        { ending: "a", tableNames: ["a_ae"] },
+        { ending: "ae", tableNames: ["a_ae"] },
+        { ending: "ai", tableNames: ["a_ae"] },
+        { ending: "arum", tableNames: ["a_ae"] },
+      ],
+      new Map([
+        [
+          "a_ae",
+          new Map([
+            ["a", [{ ending: "a", grammaticalData: ["nom/voc", "sg"] }]],
+            [
+              "ae",
+              [
+                { ending: "ae", grammaticalData: ["gen", "sg"] },
+                { ending: "ae", grammaticalData: ["nom/voc", "pl"] },
+              ],
+            ],
+            ["arum", [{ ending: "a_rum", grammaticalData: ["gen", "pl"] }]],
+            [
+              "ai",
+              [
+                {
+                  ending: "a_i_",
+                  grammaticalData: ["gen", "sg"],
+                  tags: ["poetic"],
+                },
+              ],
+            ],
+          ]),
+        ],
+      ]),
+    ]);
   });
 });
