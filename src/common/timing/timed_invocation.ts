d90ecd5117a37ba9648048ecc60ddefb86b75c61
Nitin Prasad <nkprasa@gmail.com>
2024-09-28 10:57:19 -0500

    Add async timing helper

--- src/common/timing/timed_invocation.ts
+++ src/common/timing/timed_invocation.ts
@@ -1,6 +1,19 @@
+/* istanbul ignore file */
+
 export function timed<T>(func: () => T, tag?: string): T {
   const start = performance.now();
   const result = func();
+  const runtime = (performance.now() - start).toFixed(2);
+  console.debug(`${tag || "Runtime"}: ${runtime} ms`);
+  return result;
+}
+
+export async function timedAsync<T>(
+  func: () => Promise<T> | T,
+  tag?: string
+): Promise<T> {
+  const start = performance.now();
+  const result = await func();
   const runtime = (performance.now() - start).toFixed(2);
   console.debug(`${tag || "Runtime"}: ${runtime} ms`);
   return result;
