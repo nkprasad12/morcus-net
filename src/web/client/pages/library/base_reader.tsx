e438e452e65bf399f2576c8e9a511e689d456bbd
Nitin Prasad <nkprasa@gmail.com>
2024-10-14 12:15:16 -0500

    Allow drawer to be moved on non-touch devices.

--- src/web/client/pages/library/base_reader.tsx
+++ src/web/client/pages/library/base_reader.tsx
@@ -16,7 +16,6 @@ import { StyleContext } from "@/web/client/styling/style_context";
 import React, {
   CSSProperties,
   PropsWithChildren,
-  TouchEventHandler,
   useEffect,
   useState,
   useCallback,
@@ -215,6 +214,16 @@ export function BaseReader<
 // The maximum size of the drawer as a ratio of the screen height.
 const DRAWER_MAX_SIZE = 0.6;
 
+function eventConsumer<E extends Event>(handler: (e: E) => boolean) {
+  return (e: E) => {
+    const handled = handler(e);
+    if (handled) {
+      e.preventDefault();
+      e.stopPropagation();
+    }
+  };
+}
+
 function DragHelper(
   props: PropsWithChildren<{
     currentHeight: number;
@@ -225,39 +234,61 @@ function DragHelper(
   const dragStartY = React.useRef<number | undefined>(undefined);
   const dragStartHeight = React.useRef<number | undefined>(undefined);
 
-  const onDrag: TouchEventHandler = (e) => {
-    const currentY = e.targetTouches[0].clientY;
-    if (dragStartY.current === undefined) {
-      dragStartY.current = currentY;
-    }
-    if (dragStartHeight.current === undefined) {
-      // We didn't get the onTouchStart callback yet, so wait for it.
-      return;
-    }
-    const offset = dragStartY.current - currentY;
-    const proposed = dragStartHeight.current + offset;
-    const max = window.innerHeight * DRAWER_MAX_SIZE;
-    const newHeight = proposed > max ? max : proposed < 0 ? 0 : proposed;
-    if (newHeight === 0 || Math.abs(currentHeight - newHeight) >= 3) {
-      setCurrentHeight(newHeight);
-    }
-  };
+  const onDrag = useCallback(
+    (currentY: number) => {
+      if (dragStartHeight.current === undefined) {
+        // We didn't get the onTouchStart callback yet, so wait for it.
+        return false;
+      }
+      setCurrentHeight((oldHeight) => {
+        if (dragStartY.current === undefined) {
+          dragStartY.current = currentY;
+        }
+        if (dragStartHeight.current === undefined) {
+          return oldHeight;
+        }
+        const offset = dragStartY.current - currentY;
+        const proposed = dragStartHeight.current + offset;
+        const max = window.innerHeight * DRAWER_MAX_SIZE;
+        const newHeight = proposed > max ? max : proposed < 0 ? 0 : proposed;
+        const largeChange = Math.abs(oldHeight - newHeight) >= 3;
+        return newHeight === 0 || largeChange ? newHeight : oldHeight;
+      });
+      return true;
+    },
+    [setCurrentHeight]
+  );
+
+  const onDragEnd = useCallback(() => {
+    const result =
+      dragStartHeight.current !== undefined || dragStartY.current !== undefined;
+    dragStartY.current = undefined;
+    dragStartHeight.current = undefined;
+    return result;
+  }, []);
+
+  useEffect(() => {
+    const moveHandler = eventConsumer((e: MouseEvent) => onDrag(e.clientY));
+    document.addEventListener("mousemove", moveHandler);
+    const upHandler = eventConsumer((_e: Event) => onDragEnd());
+    document.addEventListener("mouseup", upHandler);
+    return () => {
+      document.removeEventListener("mousemove", moveHandler);
+      document.removeEventListener("mouseup", upHandler);
+    };
+  }, [onDrag, onDragEnd]);
 
   const onDragStart = () => {
     dragStartHeight.current = currentHeight;
   };
 
-  function onDragEnd() {
-    dragStartY.current = undefined;
-    dragStartHeight.current = undefined;
-  }
-
   return (
     <div
       style={{ touchAction: "none" }}
+      onMouseDown={onDragStart}
       onTouchStart={onDragStart}
       onTouchEnd={onDragEnd}
-      onTouchMove={onDrag}>
+      onTouchMove={(e) => onDrag(e.targetTouches[0].clientY)}>
       {props.children}
     </div>
   );
