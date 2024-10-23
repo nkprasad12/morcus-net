47b8b5f65c85eff2f2f47989ea93153573a61574
Nitin Prasad <nkprasa@gmail.com>
2024-10-23 11:21:29 -0500

    Update pre-push script to actually exist on errors.

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,7 @@
-
 module.exports = {
   modulePaths: ["./src"],
   moduleNameMapper: {
-    "^@/(.+)$": "<rootDir>/src/$1"
+    "^@/(.+)$": "<rootDir>/src/$1",
   },
   collectCoverageFrom: ["src/**/*.ts", "src/**/*.tsx"],
   transform: {
