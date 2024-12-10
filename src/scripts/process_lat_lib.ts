40b10c6595fee71940e289a0cf496311497c7cab
Nitin Prasad <nkprasa@gmail.com>
2024-12-09 22:23:27 -0600

    Automatically use all works if we have a lib xml root

--- src/scripts/process_lat_lib.ts
+++ src/scripts/process_lat_lib.ts
@@ -2,8 +2,12 @@
 
 import fs from "fs";
 import * as dotenv from "dotenv";
-import { processLibrary } from "@/common/library/process_library";
+import {
+  ALL_SUPPORTED_WORKS,
+  processLibrary,
+} from "@/common/library/process_library";
 import { LIB_DEFAULT_DIR } from "@/common/library/library_lookup";
+import { envVar } from "@/common/env_vars";
 dotenv.config();
 
 const startTime = performance.now();
@@ -14,7 +18,13 @@ try {
   console.debug(e);
 }
 fs.mkdirSync(LIB_DEFAULT_DIR, { recursive: true });
-processLibrary(LIB_DEFAULT_DIR);
+
+const LIB_XML_ROOT = envVar("LIB_XML_ROOT", "unsafe");
+const worksList =
+  LIB_XML_ROOT === undefined
+    ? undefined
+    : ALL_SUPPORTED_WORKS.map((work) => `${LIB_XML_ROOT}/${work}`);
+processLibrary(LIB_DEFAULT_DIR, worksList);
 
 const runTime = Math.round(performance.now() - startTime);
 console.log(`Latin library processing runtime: ${runTime} ms.`);
