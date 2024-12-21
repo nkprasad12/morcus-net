260f2bd595b5f77ed4bf46355063f713a151a6c2
Nitin Prasad <nkprasa@gmail.com>
2024-12-21 17:35:29 -0600

    Add parser for roman numerals

--- src/morceus/numerals/roman_numerals.test.ts
+++ src/morceus/numerals/roman_numerals.test.ts
@@ -1,13 +1,36 @@
-import { isRomanNumeral } from "@/morceus/numerals/roman_numerals";
+import { parseRomanNumeral } from "@/morceus/numerals/roman_numerals";
 
-describe("isRomanNumeral", () => {
-  it("Rejects invalid", () => {
-    expect(isRomanNumeral("xi V")).toBe(false);
-    expect(isRomanNumeral("CFD")).toBe(false);
+describe("parseRomanNumeral", () => {
+  it("Rejects illegal characters", () => {
+    expect(parseRomanNumeral("xi V")).toBeUndefined();
+    expect(parseRomanNumeral("CFD")).toBeUndefined();
+    expect(parseRomanNumeral("K")).toBeUndefined();
+    expect(parseRomanNumeral("KI")).toBeUndefined();
   });
 
-  it("Accepts valid numerals", () => {
-    expect(isRomanNumeral("XiV")).toBe(true);
-    expect(isRomanNumeral("DDCC")).toBe(true);
+  it("Handles single digit numerals", () => {
+    expect(parseRomanNumeral("X")).toBe(10);
+    expect(parseRomanNumeral("v")).toBe(5);
+  });
+
+  it("Rejects multiple subtractands", () => {
+    expect(parseRomanNumeral("iiv")).toBeUndefined();
+    expect(parseRomanNumeral("mxxcv")).toBeUndefined();
+  });
+
+  it("handles subtractands", () => {
+    expect(parseRomanNumeral("MXCIV")).toBe(1094);
+    expect(parseRomanNumeral("IX")).toBe(9);
+  });
+
+  it("handles equal in a row", () => {
+    expect(parseRomanNumeral("MCCVII")).toBe(1207);
+    expect(parseRomanNumeral("CCLI")).toBe(251);
+  });
+
+  it("parses misc number", () => {
+    expect(parseRomanNumeral("MDCLXVI")).toBe(1666);
+    expect(parseRomanNumeral("MMMM")).toBe(4000);
+    expect(parseRomanNumeral("III")).toBe(3);
   });
 });
