cc25886a507bac9533e5fd8fd737d72161c0512d
Nitin Prasad <nkprasa@gmail.com>
2025-01-22 09:52:18 -0600

    Auto-detect and use bun if available.

--- morcus.sh
+++ morcus.sh
@@ -1,11 +1,31 @@
 #!/bin/bash
 
-if echo "$*" | grep -e "--bun" -q
-then
+has_param() {
+    local term="$1"
+    shift
+    for arg; do
+        if [[ $arg == "$term" ]]; then
+            return 0
+        fi
+    done
+    return 1
+}
+
+COMMAND="npm run ts-node --transpile_only"
+BUN_FLAG="0"
+ARGS="$@"
+
+if has_param '--node' "$@"; then
+  # If node is explicitly specified, don't try anything else.
+  true
+elif has_param '--bun' "$@"; then
   COMMAND="bun"
   BUN_FLAG="1"
-else
-  COMMAND="npm run ts-node --transpile_only"
-  BUN_FLAG="0"
+elif command -v bun 2>&1 >/dev/null; then
+  echo -e "\033[0;31mNote: bun detected, so automatically using bun. Pass --node to override this.\033[0m"
+  COMMAND="bun"
+  BUN_FLAG="1"
+  ARGS="$@ --bun"
 fi
-export BUN=$BUN_FLAG && $COMMAND src/scripts/run_morcus.ts -- "$@"
+
+export BUN=$BUN_FLAG && $COMMAND src/scripts/run_morcus.ts -- $ARGS
