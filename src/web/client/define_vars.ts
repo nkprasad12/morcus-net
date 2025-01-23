b6595a53e194076d5d6f78ceaab6780c74979082
Nitin Prasad <nkprasa@gmail.com>
2025-01-23 09:26:57 -0600

    Remove dynamic `DEFAULT_EXPERIMENTAL_MODE`. - It will always be off by default.

--- src/web/client/define_vars.ts
+++ src/web/client/define_vars.ts
@@ -1,6 +1,5 @@
 declare const COMMIT_HASH: string;
 declare const BUILD_DATE: string;
-declare const DEFAULT_EXPERIMENTAL_MODE: boolean;
 
 export function tryOr<T>(f: () => T, fallback: T): T {
   try {
@@ -16,8 +15,4 @@ export function getCommitHash(): string {
 
 export function getBuildDate(): string {
   return tryOr(() => BUILD_DATE.trim(), "undefined");
-}
-
-export function defaultExperimentalMode(): boolean {
-  return tryOr(() => DEFAULT_EXPERIMENTAL_MODE, false);
 }
