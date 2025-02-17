f935b94031d58dfa08a0b1c20da52622b990edf9
Nitin Prasad <nkprasa@gmail.com>
2025-02-17 09:40:47 -0600

    Update client performance test

--- src/integration/performance/client_performance.test.ts
+++ src/integration/performance/client_performance.test.ts
@@ -1,10 +1,41 @@
 /* istanbul ignore file */
 
 import { test, expect, type Page, type CDPSession } from "@playwright/test";
-import { checkPresent } from "@/common/assert";
+import { assert, checkPresent } from "@/common/assert";
 import fs from "fs";
 import { arrayMap } from "@/common/data_structures/collect_map";
 import { repeatedTest } from "@/integration/utils/playwright_utils";
+import { isArray, isNumber } from "@/web/utils/rpc/parsing";
+
+type ClientPerfData = { name: string; value: number }[];
+
+const MEM_METRICS = new Set(["JSHeapUsedSize", "JSHeapTotalSize"]);
+const PERF_METRICS = new Set(["LayoutDuration", "RecalcStyleDuration"]);
+const ALL_METRICS = new Set([...MEM_METRICS, ...PERF_METRICS]);
+const RAW_METRICS_DIR = "e2e_metrics/raw_data";
+
+function averageMetrics(allMetrics: ClientPerfData[]): Record<string, number> {
+  const byKey: Record<string, number[]> = {};
+  for (const singleRunMetrics of allMetrics) {
+    for (const metrics of singleRunMetrics) {
+      if (!ALL_METRICS.has(metrics.name)) {
+        continue;
+      }
+      if (byKey[metrics.name] === undefined) {
+        byKey[metrics.name] = [];
+      }
+      byKey[metrics.name].push(metrics.value);
+    }
+  }
+  const keys = Object.keys(byKey);
+  const result: Record<string, number> = {};
+  for (const key of keys) {
+    const allOfKey = byKey[key];
+    assert(isArray(isNumber)(allOfKey), JSON.stringify(allOfKey));
+    result[key] = allOfKey.reduce((a, b) => a + b, 0) / allOfKey.length;
+  }
+  return result;
+}
 
 test.describe("Client Performance Tests", () => {
   test.skip(
@@ -12,7 +43,7 @@ test.describe("Client Performance Tests", () => {
     "Performance tests rely on Chrome DevTools APIs to get data."
   );
 
-  const allMetrics = arrayMap<string, object>();
+  const allMetrics = arrayMap<string, ClientPerfData>();
   let client: CDPSession | null = null;
 
   test.beforeEach(async ({ page }) => {
@@ -31,16 +62,23 @@ test.describe("Client Performance Tests", () => {
     if (browserName !== "chromium") {
       return;
     }
-    fs.promises.writeFile(
-      `${browserName}-${viewport?.width}x${viewport?.height}.metrics.json`,
+    const rawName = `${browserName}-${viewport?.width}x${viewport?.height}.metrics.json`;
+    await fs.promises.mkdir(RAW_METRICS_DIR, { recursive: true });
+    await fs.promises.writeFile(
+      `${RAW_METRICS_DIR}/${rawName}`,
       JSON.stringify([...allMetrics.map.entries()], undefined, 2)
     );
+    for (const [key, metrics] of allMetrics.map.entries()) {
+      console.log(`Metrics for ${JSON.parse(key)}:`);
+      console.log(JSON.stringify(averageMetrics(metrics), undefined, 2));
+    }
     allMetrics.map.clear();
   });
 
   async function collectMetrics(page: Page, tag: string) {
     const metrics = await client!.send("Performance.getMetrics");
-    const size = checkPresent(page.viewportSize()?.width) < 400;
+    const size =
+      checkPresent(page.viewportSize()?.width) < 400 ? "small" : "large";
     const key = JSON.stringify([size, tag]);
     allMetrics.add(key, metrics.metrics);
   }
