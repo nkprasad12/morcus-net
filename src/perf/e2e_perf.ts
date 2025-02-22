9844cf792f5265b98508df5b0b39f5e5381d5d43
Nitin Prasad <nkprasa@gmail.com>
2025-02-21 22:02:59 -0600

    Add cc ignore for e2e performance test utils (they aren't run in unit tests)

--- src/perf/e2e_perf.ts
+++ src/perf/e2e_perf.ts
@@ -1,3 +1,5 @@
+/* istanbul ignore file */
+
 const E2E_METRICS_ROOT = "e2e_metrics";
 export const E2E_REPORTS_DIR = `${E2E_METRICS_ROOT}/reports`;
 export const E2E_RAW_METRICS_DIR = `${E2E_METRICS_ROOT}/raw`;
