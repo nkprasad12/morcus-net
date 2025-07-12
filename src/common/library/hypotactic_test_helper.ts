b5c314dc9f1f66c5a0c7c5178468fe58ffc26269
Nitin Prasad <nkprasa@gmail.com>
2025-07-11 22:13:46 -0500

    Fix race condition in setting up fake hypotactic data.

--- src/common/library/hypotactic_test_helper.ts
+++ src/common/library/hypotactic_test_helper.ts
@@ -4,29 +4,32 @@ import * as zlib from "zlib";
 
 import { replaceEnvVar } from "@/common/test_helpers";
 
-const TEST_DATA_ROOT = "testdata/hypotactic";
+const UNCOMPRESSED_DATA_ROOT = "testdata/hypotactic";
 
 export function setupFakeHypotacticData() {
-  replaceEnvVar("HYPOTACTIC_ROOT", TEST_DATA_ROOT);
+  const testDataRoot = fs.mkdtempSync("hypotactic-testdata-");
+  replaceEnvVar("HYPOTACTIC_ROOT", testDataRoot);
 
   beforeAll(() => {
-    const files = fs.readdirSync(TEST_DATA_ROOT);
+    const files = fs.readdirSync(UNCOMPRESSED_DATA_ROOT);
     const jsonFiles = files.filter((file) => file.endsWith(".json"));
 
     for (const jsonFile of jsonFiles) {
-      const jsonFilePath = path.join(TEST_DATA_ROOT, jsonFile);
+      const jsonFilePath = path.join(UNCOMPRESSED_DATA_ROOT, jsonFile);
       const content = fs.readFileSync(jsonFilePath);
       const gzippedContent = zlib.gzipSync(content);
-      fs.writeFileSync(`${jsonFilePath}.gz`, gzippedContent);
+      fs.writeFileSync(
+        path.join(testDataRoot, `${jsonFile}.gz`),
+        gzippedContent
+      );
     }
   });
 
   afterAll(() => {
-    const files = fs.readdirSync(TEST_DATA_ROOT);
-    for (const file of files) {
-      if (file.endsWith(".json.gz")) {
-        fs.unlinkSync(path.join(TEST_DATA_ROOT, file));
-      }
+    try {
+      fs.rmSync(testDataRoot, { recursive: true, force: true });
+    } catch (error) {
+      console.error("Error cleaning up test data:", error);
     }
   });
 }
