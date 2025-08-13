f06cfbd2c62fe44706e9a59e674e47047d956aad
Nitin Prasad <nkprasa@gmail.com>
2025-08-13 10:10:36 -0500

    Check in basic implementation of corpus query in rust. There are some bugs.

--- src/rust/src/main.rs
+++ src/rust/src/main.rs
@@ -1,5 +1,6 @@
 mod bitmask_utils;
 mod common;
+mod corpus_query_engine;
 mod corpus_serialization;
 mod packed_arrays;
 mod packed_index_utils;
@@ -9,5 +10,40 @@ const CORPUS_ROOT: &str = "../../build/corpus/latin_corpus.json";
 fn main() {
     let corpus = corpus_serialization::deserialize_corpus(CORPUS_ROOT)
         .expect("Failed to deserialize corpus");
-    println!("Corpus loaded with {} indices", corpus.indices.keys().cloned().collect::<Vec<String>>().join(", "));
+    let engine =
+        corpus_query_engine::CorpusQueryEngine::new(corpus).expect("Failed to create query engine");
+    let query: corpus_query_engine::CorpusQuery = corpus_query_engine::CorpusQuery {
+        parts: vec![
+            corpus_query_engine::CorpusQueryPart {
+                token: corpus_query_engine::CorpusQueryAtom::Lemma(corpus_query_engine::LemmaQuery {
+                    lemma: "do".to_string(),
+                }),
+            },
+            corpus_query_engine::CorpusQueryPart {
+                token: corpus_query_engine::CorpusQueryAtom::Word(corpus_query_engine::WordQuery {
+                    word: "oscula".to_string(),
+                }),
+            },
+            corpus_query_engine::CorpusQueryPart {
+                token: corpus_query_engine::CorpusQueryAtom::Inflection(corpus_query_engine::InflectionQuery {
+                    category: "case".to_string(),
+                    value: "3".to_string(),
+                }),
+            },
+        ],
+    };
+    let results = engine
+        .query_corpus(&query, 0, Some(10))
+        .expect("Query failed");
+    println!(
+        "Showing results {}-{} of {} matches:\n",
+        results.page_start + 1,
+        results.page_start + results.matches.len(),
+        results.total_results
+    );
+    for m in results.matches {
+        println!("  {} - {} {}", m.author, m.work_name, m.section);
+        println!("    {}*{}*{}", m.left_context, m.text, m.right_context,);
+        println!();
+    }
 }
